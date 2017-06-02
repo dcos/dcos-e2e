14f90b9f0dd443352ad74cc55e231bb53f48ac31
Adam Dangoor <adamdangoor@gmail.com>
2017-06-02 03:55:58 +0100

    Link to issue that code works around

--- src/dcos_e2e/_dcos_docker.py
+++ src/dcos_e2e/_dcos_docker.py
@@ -187,6 +187,7 @@ class DCOS_Docker_Cluster:  # pylint: disable=invalid-name
         Creating clusters involves creating temporary installer containers.
         These containers can conflict in name.
         If a conflict occurs, retry.
+        This works around https://jira.mesosphere.com/browse/DCOS_OSS-1192.
         """
         # The error substring differs on different versions of Docker.
         conflict_error_substring = 'Conflict. The container name'
