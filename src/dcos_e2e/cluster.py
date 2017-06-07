ce78b2505a9bd6e75f150f17a5651dc6af8d2468
Adam Dangoor <adamdangoor@gmail.com>
2017-06-07 16:59:14 +0100

    Use a better default os user

--- src/dcos_e2e/cluster.py
+++ src/dcos_e2e/cluster.py
@@ -94,12 +94,13 @@ class Cluster(ContextDecorator):
 
         dcos_url = 'http://' + str(web_host.ip_address)
         auth_user = DcosUser(credentials=CI_CREDENTIALS)
+        default_os_user = 'root'
         api_session = DcosApiSession(
             dcos_url=dcos_url,
             masters=masters_ip_addresses,
             slaves=agents_ip_addresses,
             public_slaves=public_agent_ip_addresses,
-            default_os_user=self._superuser_username,
+            default_os_user=default_os_user,
             auth_user=auth_user,
         )
         # Without the following line, if we use a CA certificate, e.g. in a
