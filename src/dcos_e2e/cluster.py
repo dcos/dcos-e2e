20f490f8f2f73d3f5e8d56ddea3507f2ddcfcaa5
Adam Dangoor <adamdangoor@gmail.com>
2017-06-16 18:32:10 +0100

    Decode bytes before loading

--- src/dcos_e2e/cluster.py
+++ src/dcos_e2e/cluster.py
@@ -70,7 +70,8 @@ class Cluster(ContextDecorator):
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE,
         )
-        variant = json.loads(str(version_output.stdout))['variant']
+        import pdb; pdb.set_trace()
+        variant = json.loads(version_output.stdout.decode())['variant']
         self._enterprise_cluster = variant == 'ee'
 
         extra_config = dict(extra_config or {})
