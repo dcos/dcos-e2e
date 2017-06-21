8036bfab9b3805c044a2c76472c2a403be9f0099
Adam Dangoor <adamdangoor@gmail.com>
2017-06-21 17:56:11 +0100

    Fix typo

--- src/dcos_e2e/cluster.py
+++ src/dcos_e2e/cluster.py
@@ -93,7 +93,7 @@ class Cluster(ContextDecorator):
         """
         Wait until DC/OS has started and all nodes have joined the cluster.
         """
-        diagnostics_args = ['/opt/mesosphere/bin/.3dt', '--diag']
+        diagnostics_args = ['/opt/mesosphere/bin/./3dt', '--diag']
 
         for node in self.masters:
             node.run_as_root(
