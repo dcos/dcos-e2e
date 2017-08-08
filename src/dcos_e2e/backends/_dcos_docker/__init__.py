627fd3d1ffea56d0cf7b944c9155de4114d7c844
Adam Dangoor <adamdangoor@gmail.com>
2017-08-08 13:47:44 +0100

    Create genconf dir if it does not exist

--- src/dcos_e2e/backends/_dcos_docker/__init__.py
+++ src/dcos_e2e/backends/_dcos_docker/__init__.py
@@ -155,19 +155,19 @@ class DCOS_Docker_Cluster(ClusterManager):  # pylint: disable=invalid-name
             ignore=ignore_patterns('dcos_generate_config.sh'),
         )
 
-        # Files in the DC/OS Docker directory's `genconf.src` directory are
-        # mounted to the installer at `/genconf`.
+        # Files in the DC/OS Docker directory's `genconf` directory are mounted
+        # to the installer at `/genconf`.
         # Therefore, every file which we want to copy to `/genconf` on the
         # installer is put into the genconf directory in DC/OS Docker.
         # The way to fix this if we want to be able to put files anywhere is
         # to add an variable to `dcos_generate_config.sh.in` which allows
         # `-v` mounts.
         # Then `INSTALLER_MOUNTS` can be added to DC/OS Docker.
+        genconf_dir = self._path / 'genconf'
+        os.makedirs(name=str(genconf_dir), exist_ok=True)
         for host_path, installer_path in files_to_copy_to_installer.items():
             relative_installer_path = installer_path.relative_to('/genconf')
-            destination_path = (
-                self._path / 'genconf.src' / relative_installer_path
-            )
+            destination_path = genconf_dir / relative_installer_path
             copyfile(src=str(host_path), dst=str(destination_path))
 
         extra_genconf_config = ''
