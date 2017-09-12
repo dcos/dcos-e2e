ff13b7f980e1039eb90a4eea8321e8f8b293b5e4
Adam Dangoor <adamdangoor@gmail.com>
2017-09-12 23:57:17 +0100

    Ignore debug logs

--- tests/test_enterprise.py
+++ tests/test_enterprise.py
@@ -38,7 +38,7 @@ class TestEnterpriseIntegrationTests:
             generate_config_path=enterprise_artifact,
             cluster_backend=cluster_backend,
             extra_config=extra_config,
-            log_output_live=False,
+            log_output_live=True,
         ) as cluster:
             # No error is raised with a successful command.
             cluster.run_integration_tests(
@@ -81,7 +81,7 @@ class TestWaitForDCOS:
             generate_config_path=enterprise_artifact,
             cluster_backend=cluster_backend,
             extra_config=extra_config,
-            log_output_live=False,
+            log_output_live=True,
         ) as cluster:
             (master, ) = cluster.masters
             cluster.wait_for_dcos()
