1391abc98ec6872b8197b6b2b11e707ed780f395
Adam Dangoor <adamdangoor@gmail.com>
2017-09-20 15:51:02 +0100

    Squashed 'src/dcos_e2e/backends/_dcos_docker/dcos_docker/' changes from 4e660b1..89ebb80
    89ebb80 Merge pull request #63 from mellenburg/no_janitor
    7bf01eb Remove janitor in oinker for DC/OS 1.10
    1a4d460 Merge pull request #62 from mellenburg/fix_pkg_json
    f6e4097 Fix oinker json
    2b73e20 Fix package json
    b7e556c Upgrade dcos-postlfight to use dcos-diagnostics checks
    3066f72 Fix make build-all
    c2491d9 jess/coreos doesn't exist - found replacement
    95518bc Install jq into node containers (for dcos-postflight to use)
    88f8f44 Merge pull request #61 from adamtheturtle/set-master-hosts-DCOS-17636
    3944f17 Set master hosts for tests
    d09806c add ssh-keygen reqirement
    511d3d9 Merge pull request #60 from adamtheturtle/404-errors-DCOS-16482-2
    1609557 Add --fail to more curl commands
    16597b9 Pin completion scripts for each version
    74958ba fix quotes not being escaped
    40cf0b8 Use teamcity-messages to fold unbuffered stdout/stderr
    2af3751 Clean all running dcos containers, even if more than currently configured
    4d3eefe make genconf/serve/ before creating docker containers
    12edc71 Build status list
    3abd118 Update build status badges
    44bcd8e Avoid --master-proxy with --option IdentityFile
    f988de3 Support dcos-cli 0.5.3 (uninstall --yes)
    0d8f3ab Check HOME instead of assuming root users have HOME set to /root
    058b526 Replace HOME_VOLUMES with CUSTOM_VOLUMES
    1dcb52e Only ssh-keygen -R if known_hosts exists
    acec70f Fix ssh-agent cleanup in ssh-wrapper
    b89c01a Export all log lines by default
    0b99161 Add log extraction to dcos-test-smoke
    dc208ee Add dcos-log dump to integration and e2e tests
    9e74df1 Add dcos-logs.sh to export node logs
    cf7857b Move RESOLVERS lookup to ./configure
    963c182 refactor make test
    8455335 only linbreak before pending if using progress dots
    d40ca13 fix postflight bugs
    bf021e5 Update dcos-postflight to add --progress=time
    bb17ca5 exit postflight on timeout
    d99f5f6 use HOME instead of ~ so make test works as root
    ebd6daf integration tests need 2 agents, not 2 masters
    f11b585 fix test-integration make-config.mk path
    9cdb402 clean up containers used in configure
    6e5de30 Add ssh-wrapper to start/stop session ssh-agent
    8597e11 dd ingration test script (no smoke test)
    b16e911 Add smoke test that just runs the oinker test (for faster feedback)
    777c002 document memory requirement
    236a183 configure cleanup
    e2badc8 Update readme
    d82bc4f Merge pull request #58 from dcos/configure-cli
    7a6d91c Merge pull request #57 from asridharan/DCOS_OSS-1398
    a2815db Merge pull request #59 from bamarni/patch-1
    246a85c readme: typo fix
    b521fa5 sed's -i isn't in POSIX. So use two commands to support multiple variants of sed.
    d5f681f Fix sed commands to not use the -E suffix when using extended regex
    e75240b minor comment change
    11ac7a1 Refactor volume variables
    41b4a32 fix common.mk import
    9a78016 Add ./configure
    2f6b840 Fixed instructions for forcing dcos-docker to use `docker 1.13.1`.
    
    git-subtree-dir: src/dcos_e2e/backends/_dcos_docker/dcos_docker
    git-subtree-split: 89ebb80503381efbf8f20d36985a926e2b4d4bdd

--- ci/test-integration.sh
+++ ci/test-integration.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 
-# Performs End To End (e2e) testing of DC/OS Docker.
+# Performs smoke testing of DC/OS Docker.
 #
 # Options:
-#   DCOS_VERSION Version of the supplied DC/OS installer (defaults to the latest stable version)
 #   LOG_LINES    Number of log lines to export for each node (exports all, if unset)
 #
 # Usage:
@@ -36,12 +35,6 @@ echo "${BASH_VERSINFO[@]}"
 
 # Check for running containers
 docker ps
-
-# Default to latest known version unless DCOS_VERSION is specified
-if [[ -z "${DCOS_VERSION:-}" ]]; then
-  bash dcos_generate_config.sh --version
-  DCOS_VERSION="$(bash dcos_generate_config.sh --version | jq -r '.version')"
-fi
 
 # Destroy all containers
 make clean
@@ -99,50 +92,17 @@ make hosts
 # Test API (unauthenticated)
 curl --fail --location --silent --show-error --verbose http://m1.dcos/dcos-metadata/dcos-version.json
 
-# Install CLI
-DCOS_CLI="$(ci/dcos-install-cli.sh)"
-echo "${DCOS_CLI}"
-
-# Delete CLI on exit
-function cleanup3() {
-  # only use sudo if required
-  if [[ -w "$(dirname "${DCOS_CLI}")" ]]; then
-    rm -rf "${DCOS_CLI}"
-  else
-    sudo rm -rf "${DCOS_CLI}"
-  fi
-  cleanup2
-}
-trap cleanup3 EXIT
-
-# Create User
-DCOS_USER="test@example.com"
-ci/dcos-create-user.sh "${DCOS_USER}"
-
-# Login
-DCOS_ACS_TOKEN="$(ci/dcos-login.sh "${DCOS_USER}")"
-dcos config set core.dcos_acs_token "${DCOS_ACS_TOKEN}"
-
-# Install & test Oinker
-ci/test-oinker.sh
-
-# Detect URL
-DCOS_URL="$(dcos config show core.dcos_url)"
-
-# Test GUI (authenticated)
-curl --fail --location --silent --show-error --verbose -H "Authorization: token=${DCOS_ACS_TOKEN}" ${DCOS_URL} -o /dev/null
-
 # Add test user (required to be added when not the first user)
 # TODO: only required for OSS DC/OS
 ci/dcos-create-user.sh "albert@bekstil.net"
 
 # Delete CLI on exit
-function cleanup4() {
+function cleanup3() {
   # Copy out test results
   docker cp dcos-docker-master1:/opt/mesosphere/active/dcos-integration-test/test-junit.xml test-junit.xml || true
-  cleanup3
+  cleanup2
 }
-trap cleanup4 EXIT
+trap cleanup3 EXIT
 
 # Integration tests
 make test
