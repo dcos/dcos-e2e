0769c060dc34ad5e6463a77348bff78fe95a8c8f
Adam Dangoor <adamdangoor@gmail.com>
2017-10-10 18:04:27 +0100

    Squashed 'src/dcos_e2e/backends/_dcos_docker/dcos_docker/' changes from 89ebb80..ecf83cf
    ecf83cf Update the oinker example to work on more environments
    3cef998 Add section on hostnames
    0a89a6b Fix routes being printed as files instead of to /etc/hosts
    8996dd7 Update oinker example
    2903ecd Add await-url-health to test MLB routing
    86aa204 Fix integration tests on Jenkins
    82a07a0 Upgrade smoke test to use Cassandra 2.0
    
    git-subtree-dir: src/dcos_e2e/backends/_dcos_docker/dcos_docker
    git-subtree-split: ecf83cf12017302b2de3585e25ebe4745f1f7075

--- ci/await-app-health.sh
+++ ci/await-app-health.sh
@@ -4,11 +4,9 @@
 # Times out after 5 minutes.
 #
 # Usage:
-# $ ci/test-app-health.sh <app-id> [timeout-seconds]
+# $ ci/await-app-health.sh <app-id> [timeout-seconds]
 
-set -o errexit
-set -o nounset
-set -o pipefail
+set -o errexit -o nounset -o pipefail
 
 project_dir=$(cd "$(dirname "${BASH_SOURCE}")/.." && pwd -P)
 cd "${project_dir}"
@@ -22,7 +20,6 @@ echo >&2  "Found app (${APP_ID}) instances: ${INSTANCES}"
 
 SUCCESS=false
 START_TIME=${SECONDS}
-MAX_ELAPSED=300 #5min
 while [[ $((${SECONDS} - ${START_TIME})) -lt ${MAX_ELAPSED} ]]; do
   echo "Polling app (${APP_ID}) instance health..."
   if [[ "$(dcos marathon app show "${APP_ID}" | jq '.tasksHealthy')" == "${INSTANCES}" ]]; then
@@ -31,9 +28,9 @@ while [[ $((${SECONDS} - ${START_TIME})) -lt ${MAX_ELAPSED} ]]; do
   fi
   sleep 5
 done
-if [[ "${SUCCESS}" = true ]]; then
+if [[ "${SUCCESS}" == true ]]; then
   echo >&2 "App (${APP_ID}) healthy"
 else
-  echo >&2 "App (${APP_ID}) unhealthy -- Timed out after 5 minutes."
+  echo >&2 "App (${APP_ID}) unhealthy -- Timed out after ${MAX_ELAPSED} seconds."
   exit 1
 fi
