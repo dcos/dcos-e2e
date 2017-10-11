c4f4e0028c4d6a1cac6ae8c17b0527bdf3cd296d
Adam Dangoor <adamdangoor@gmail.com>
2017-10-11 01:33:44 +0100

    Add test for copy to installer

--- tests/test_enterprise.py
+++ tests/test_enterprise.py
@@ -7,6 +7,7 @@ import uuid
 from pathlib import Path
 
 import pytest
+import requests
 from passlib.hash import sha512_crypt
 
 from dcos_e2e.backends import ClusterBackend
@@ -48,6 +49,71 @@ class TestEnterpriseIntegrationTests:
                     'DCOS_LOGIN_PW': superuser_password,
                 },
             )
+
+
+class TestCopyFiles:
+    """
+    Tests for copying files to nodes.
+    """
+
+    def test_copy_files_to_installers(
+        self,
+        cluster_backend: ClusterBackend,
+        enterprise_artifact: Path,
+    ) -> None:
+        """
+        Files can be copied from the host to the installer nodes at creation
+        time.
+
+        The installer containers are removed shortly after creation by DC/OS
+        Docker. Therefore, we inspect the symptoms - we can use custom CA
+        certificates.
+
+        See CA certificate tests in Enterprise DC/OS for more details.
+        """
+        cert_filename = 'dcos-ca-certificate.crt'
+        key_filename = 'dcos-ca-certificate-key.key'
+
+        genconf = Path('/genconf')
+        installer_cert_path = genconf / cert_filename
+        installer_key_path = genconf / key_filename
+
+        cert_dir_on_host = Path('tests/certificates')
+        cert_path = cert_dir_on_host / cert_filename
+        ca_key_path = cert_dir_on_host / key_filename
+
+        master_key_path = Path(
+            '/var/lib/dcos/pki/tls/CA/private/custom_ca.key'
+        )
+
+        config = {
+            'superuser_username': str(uuid.uuid4()),
+            'superuser_password_hash': sha512_crypt.hash(str(uuid.uuid4())),
+            'security': 'strict',
+            'ca_certificate_path': str(installer_cert_path),
+            'ca_certificate_key_path': str(installer_key_path),
+        }
+
+        files_to_copy_to_installer = {
+            cert_path: installer_cert_path,
+            ca_key_path: installer_key_path,
+        }
+
+        with Cluster(
+            cluster_backend=cluster_backend,
+            extra_config=config,
+            generate_config_path=enterprise_artifact,
+            files_to_copy_to_installer=files_to_copy_to_installer,
+            files_to_copy_to_masters={ca_key_path: master_key_path},
+            masters=1,
+            agents=0,
+            public_agents=0,
+        ) as cluster:
+            cluster.wait_for_dcos()
+            (master, ) = cluster.masters
+            master_url = 'https://' + str(master.ip_address)
+            response = requests.get(master_url, verify=str(cert_path))
+            response.raise_for_status()
 
 
 class TestWaitForDCOS:
