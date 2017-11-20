25696559c570ebb7a15677f4936de1c04e9fe6a8
Tim Weidner <tweidner@mesosphere.io>
2017-11-20 11:06:41 +0100

    set ca cert

--- src/dcos_e2e/cluster.py
+++ src/dcos_e2e/cluster.py
@@ -104,6 +104,7 @@ class Cluster(ContextDecorator):
         auth_user = EnterpriseUser(superuser_username, superuser_password)
         cluster_args['auth_user'] = auth_user
         session = EnterpriseApiSession(**cluster_args)
+        session.set_ca_cert()
         session.wait_for_dcos()
 
     def __enter__(self) -> 'Cluster':
