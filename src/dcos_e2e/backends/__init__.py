4516e2f94d10b5df72920707250cdbb216294166
Bilal Amarni <bilal.amarni@gmail.com>
2018-02-01 15:09:02 +0100

    Add a DC/OS Launch backend
    This adds a DC/OS launch backend, it is currently not very generic as it
    was made to satisfy my specific needs in DC/OS licensing integration
    tests.
    
    Hopefully we can start to work from this.

--- src/dcos_e2e/backends/__init__.py
+++ src/dcos_e2e/backends/__init__.py
@@ -3,12 +3,14 @@ Classes to allow backend-specific configuration for cluster backend types.
 """
 
 from ._base_classes import ClusterBackend, ClusterManager
+from ._dcos_launch import DCOSLaunch
 from ._docker import Docker
 from ._existing_cluster import ExistingCluster as _ExistingCluster
 
 __all__ = [
     'ClusterBackend',
     'ClusterManager',
+    'DCOSLaunch',
     'Docker',
     '_ExistingCluster',
 ]
