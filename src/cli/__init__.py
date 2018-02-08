11bbc33714ed59913329920a16e89849030d259c
Adam Dangoor <adamdangoor@gmail.com>
2018-02-08 14:20:28 +0000

    Add entry point

--- src/cli/__init__.py
+++ src/cli/__init__.py
@@ -6,3 +6,12 @@ def dcos_docker():
     """
     Manage DC/OS clusters on Docker.
     """
+
+@click.command()
+def create():
+    """
+    Create a DC/OS cluster.
+    """
+
+if __name__ == '__main__':
+    dcos_docker()
