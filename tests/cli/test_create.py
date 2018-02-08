a30d65b148f59aba2d4a14ab973e45da527ccb53
Adam Dangoor <adamdangoor@gmail.com>
2018-02-08 13:35:07 +0000

    Progress

--- tests/cli/test_create.py
+++ tests/cli/test_create.py
@@ -7,6 +7,7 @@ from pathlib import Path
 from cli import create
 from click.testing import CliRunner
 
+
 def test_invalid_artifact_path():
     """
     XXX
@@ -15,6 +16,7 @@ def test_invalid_artifact_path():
     result = runner.invoke(create, ['/not/a/path'])
     assert result.exit_code == 0
     assert result.output == 'XXX\n'
+
 
 def test_create(oss_artifact: Path):
     """
