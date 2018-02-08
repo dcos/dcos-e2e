861f5b5c40cb0056a8f81b1ebc5340c2b70d99da
Adam Dangoor <adamdangoor@gmail.com>
2018-02-08 14:33:22 +0000

    Passing test for missing artifact

--- tests/test_cli/test_create.py
+++ tests/test_cli/test_create.py
@@ -30,4 +30,4 @@ def test_create(oss_artifact: Path):
     runner = CliRunner()
     result = runner.invoke(dcos_docker, ['create', str(oss_artifact)])
     assert result.exit_code == 0
-    assert result.output == 'Hello Peter!\n'
+    assert result.output == ''
