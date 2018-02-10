2c2cd327c043473ec61c77ebc7527664a25cc72a
Adam Dangoor <adamdangoor@gmail.com>
2018-02-10 19:05:41 +0000

    Use py35 syntax [skip ci]

--- src/dcos_e2e/distributions.py
+++ src/dcos_e2e/distributions.py
@@ -1,8 +1,10 @@
 """
 Supported distributions for DC/OS.
+
+In Python 3.6 this can use `enum.auto()`.
 """
 
-from enum import Enum, auto
+from enum import Enum
 
 
 class Distribution(Enum):
@@ -10,8 +12,8 @@ class Distribution(Enum):
     Supported distributions for DC/OS.
     """
 
-    CENTOS_7 = auto()
-    UBUNTU_16_04 = auto()
-    COREOS = auto()
-    FEDORA_23 = auto()
-    DEBIAN_8 = auto()
+    CENTOS_7 = 1
+    UBUNTU_16_04 = 2
+    COREOS = 3
+    FEDORA_23 = 4
+    DEBIAN_8 = 5
--- src/dcos_e2e/docker_versions.py
+++ src/dcos_e2e/docker_versions.py
@@ -3,9 +3,11 @@ Supported versions of Docker for DC/OS.
 
 See
 https://docs.mesosphere.com/1.10/installing/oss/custom/system-requirements/#docker
+
+In Python 3.6 this can use `enum.auto()`.
 """
 
-from enum import Enum, auto
+from enum import Enum
 
 
 class DockerVersion(Enum):
@@ -13,5 +15,5 @@ class DockerVersion(Enum):
     Supported versions of Docker for DC/OS.
     """
 
-    v1_13_1 = auto()
-    v1_11_2 = auto()
+    v1_13_1 = 1
+    v1_11_2 = 2
