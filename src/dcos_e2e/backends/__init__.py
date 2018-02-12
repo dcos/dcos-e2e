5e34689e528e8056635cdecc312f7d5df407546f
Tim Weidner <tweidner@mesosphere.io>
2018-02-12 19:13:57 +0100

    dcos-launch based AWS backend for resilience tests

--- src/dcos_e2e/backends/__init__.py
+++ src/dcos_e2e/backends/__init__.py
@@ -3,14 +3,14 @@ Classes to allow backend-specific configuration for cluster backend types.
 """
 
 from ._base_classes import ClusterBackend, ClusterManager
-from ._dcos_launch import DCOSLaunch
+from ._aws import AWS
 from ._docker import Docker
 from ._existing_cluster import ExistingCluster as _ExistingCluster
 
 __all__ = [
     'ClusterBackend',
     'ClusterManager',
-    'DCOSLaunch',
+    'AWS',
     'Docker',
     '_ExistingCluster',
 ]
---
+++ tests/backends/test_aws.py
@@ -0,0 +1,46 @@
+"""
+Tests for the Docker backend.
+"""
+
+import os
+import uuid
+from pathlib import Path
+
+from dcos_e2e.backends import AWS
+from dcos_e2e.cluster import Cluster
+from dcos_e2e.distributions import Distribution
+
+from passlib.hash import sha512_crypt
+
+
+class TestAWSBackend:
+    """
+    Tests for functionality specific to the Docker backend.
+    """
+
+    def test_cluster(self, license_key_contents: str) -> None:
+
+        superuser_username = str(uuid.uuid4())
+        superuser_password = str(uuid.uuid4())
+        config = {
+            'superuser_username': superuser_username,
+            'superuser_password_hash': sha512_crypt.hash(superuser_password),
+            'fault_domain_enabled': False,
+            'license_key_contents': license_key_contents,
+        }
+
+        aws_backend = AWS(
+            aws_key_name='default',
+            ssh_key_path=Path('/Users/tim/.ssh/default'),
+        )
+
+        with Cluster(cluster_backend=aws_backend, masters=1) as cluster:
+            cluster.install_dcos_from_url(
+                build_artifact=os.environ['INSTALLER_URL'],
+                extra_config=config,
+                log_output_live=True,
+            )
+            cluster.wait_for_dcos_ee(
+                superuser_username=superuser_username,
+                superuser_password=superuser_password,
+            )
