b18496de2685dea7e161c50809c4b9ce7a055fe7
Tim Weidner <tweidner@mesosphere.io>
2018-02-13 22:35:28 +0100

    Up-to-date with master

--- src/dcos_e2e/backends/__init__.py
+++ src/dcos_e2e/backends/__init__.py
@@ -2,8 +2,8 @@
 Classes to allow backend-specific configuration for cluster backend types.
 """
 
-from ._base_classes import ClusterBackend, ClusterManager
 from ._aws import AWS
+from ._base_classes import ClusterBackend, ClusterManager
 from ._docker import Docker
 from ._existing_cluster import ExistingCluster as _ExistingCluster
 
