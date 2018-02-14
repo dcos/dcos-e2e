f54917b82289002a9a1a409acb731c9c578c6ab2
Adam Dangoor <adamdangoor@gmail.com>
2018-02-14 02:35:14 +0000

    No need to quote now

--- src/cli/__init__.py
+++ src/cli/__init__.py
@@ -619,11 +619,11 @@ def inspect_cluster(cluster_id: str, env: bool) -> None:
     )
 
 
-@dcos_docker.command('run')
+@dcos_docker.command('run', context_settings=dict(ignore_unknown_options=True))
 @click.argument('cluster_id', type=str, callback=_validate_cluster_exists)
-@click.argument('args', type=str, nargs=-1)
+@click.argument('node_args', type=str, nargs=-1)
 # TODO actually a tuple
-def run(cluster_id: str, args: List[str]) -> None:
+def run(cluster_id: str, node_args: List[str]) -> None:
     """
     XXX
     """
@@ -634,7 +634,7 @@ def run(cluster_id: str, args: List[str]) -> None:
         'cd',
         '/opt/mesosphere/active/dcos-integration-test/',
         '&&',
-    ] + list(args)
+    ] + list(node_args)
 
     def ip_addresses(nodes: Iterable[Node]) -> str:
         return ','.join(map(lambda node: str(node.public_ip_address), nodes))
