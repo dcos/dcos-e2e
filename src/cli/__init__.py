d68c1576f607dafc7c0f3b18c1d4140d14321a7f
Adam Dangoor <adamdangoor@gmail.com>
2018-02-15 14:39:35 +0000

    Remove done notes

--- src/cli/__init__.py
+++ src/cli/__init__.py
@@ -4,8 +4,6 @@ A CLI for controlling DC/OS clusters on Docker.
 Ideas for improvements
 ----------------------
 
-* dcos_docker web
-* Document Doctor
 * Handle Custom CA Cert case, with mounts (and copy to installer)
 * brew install
 * Windows support
