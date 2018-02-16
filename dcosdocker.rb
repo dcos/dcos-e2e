223dc3eb4cc5106120718b12158ed9da85f6a1f9
Adam Dangoor <adamdangoor@gmail.com>
2018-02-16 14:47:49 +0000

    Add installation instructions

--- dcosdocker.rb
+++ dcosdocker.rb
@@ -1,19 +1,8 @@
 class Dcosdocker < Formula
   include Language::Python::Virtualenv
 
-  url "https://s3.eu-central-1.amazonaws.com/mhrabovcin/dcos-e2e.tar"
-  sha256 "ad0e722bb9b2765667d9b2acfe99a92b670c77fc9d3f41a2eda776c2b2f1ea00"
+  head "https://github.com/mesosphere/dcos-e2e.git"
 
-  #
-  # resource "six" do
-  #   url "https://pypi.python.org/packages/source/s/six/six-1.9.0.tar.gz"
-  #   sha256 "e24052411fc4fbd1f672635537c3fc2330d9481b18c0317695b46259512c91d5"
-  # end
-  #
-  # resource "parsedatetime" do
-  #   url "https://pypi.python.org/packages/source/p/parsedatetime/parsedatetime-1.4.tar.gz"
-  #   sha256 "09bfcd8f3c239c75e77b3ff05d782ab2c1aed0892f250ce2adf948d4308fe9dc"
-  # end
   resource "asn1crypto" do
     url "https://files.pythonhosted.org/packages/fc/f1/8db7daa71f414ddabfa056c4ef792e1461ff655c2ae2928a2b675bfed6b4/asn1crypto-0.24.0.tar.gz"
     sha256 "9d5c20441baf0cb60a4ac34cc447c6c189024b6b4c6cd7877034f4965c464e49"
@@ -163,6 +152,6 @@ class Dcosdocker < Formula
   depends_on "python3"
   def install
     virtualenv_install_with_resources
-    system "python3", *Language::Python.setup_install_args(libexec/"dcos-e2e-master")
+    # system "python3", *Language::Python.setup_install_args(libexec/"dcos-e2e-master")
   end
 end
