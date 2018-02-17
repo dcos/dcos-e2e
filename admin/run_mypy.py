d44a9cfb7aaac5662ba06b743b37831fc0c63e2c
Adam Dangoor <adamdangoor@gmail.com>
2018-02-17 11:52:07 +0000

    Working type hinting

---
+++ admin/run_mypy.py
@@ -0,0 +1,25 @@
+"""
+Run `mypy`, ignoring relevant errors.
+"""
+
+import subprocess
+import sys
+
+
+def main() -> None:
+    args = ['mypy', 'src/', 'tests/']
+    ignore_paths = {
+        'src/dcos_e2e/_vendor',
+    }
+    result = subprocess.run(args=args, stdout=subprocess.PIPE)
+    result_lines = result.stdout.decode().strip().split('\n')
+    error_lines = [
+        line for line in result_lines for path in ignore_paths if
+        not line.startswith(path)
+    ]
+    print('\n'.join(error_lines))
+    sys.exit(int(bool(error_lines)))
+
+
+if __name__ == '__main__':
+    main()
