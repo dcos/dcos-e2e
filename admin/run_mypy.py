bf6db7c78d8fb5be9b28ed651edb2957eec33ee0
Adam Dangoor <adamdangoor@gmail.com>
2018-02-17 12:11:35 +0000

    Update run_mypy.py

--- admin/run_mypy.py
+++ admin/run_mypy.py
@@ -14,8 +14,8 @@ def main() -> None:
     result = subprocess.run(args=args, stdout=subprocess.PIPE)
     result_lines = result.stdout.decode().strip().split('\n')
     error_lines = [
-        line for line in result_lines for path in ignore_paths if
-        not line.startswith(path)
+        line for line in result_lines
+        if not any(line.startswith(path) for path in ignore_paths)
     ]
     print('\n'.join(error_lines))
     sys.exit(int(bool(error_lines)))
