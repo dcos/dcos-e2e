ba150d7dc07f23f184511a33693f9dec1bb61760
Adam Dangoor <adamdangoor@gmail.com>
2018-02-18 17:27:07 +0000

    Fix system cmd

--- src/cli/__init__.py
+++ src/cli/__init__.py
@@ -774,6 +774,7 @@ def run(
     cluster = cluster_containers.cluster
 
     terminal_size = shutil.get_terminal_size()
+    import pdb; pdb.set_trace()
 
     environment = {
         'MASTER_HOSTS': ip_addresses(cluster.masters),
