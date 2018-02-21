e72d72e0c7b13a260276979aaae63b1585572332
Adam Dangoor <adamdangoor@gmail.com>
2018-02-21 14:07:15 +0000

    Written tests

--- tests/test_dcos_e2e/test_legacy.py
+++ tests/test_dcos_e2e/test_legacy.py
@@ -37,12 +37,65 @@ class Test_1_10:
         enterprise_1_10_artifact: Path,
     ) -> None:
         """
-        Integration tests can be run with `pytest`.
-        Errors are raised from `pytest`.
+        A DC/OS Enterprise 1.10 cluster can be started.
         """
+        superuser_username = str(uuid.uuid4())
+        superuser_password = str(uuid.uuid4())
+        config = {
+            'superuser_username': superuser_username,
+            'superuser_password_hash': sha512_crypt.hash(superuser_password),
+        }
         with Cluster(cluster_backend=cluster_backend) as cluster:
             cluster.install_dcos_from_path(
                 build_artifact=enterprise_1_10_artifact,
+                extra_config=config,
+                log_output_live=True,
+            )
+            cluster.wait_for_dcos_ee(
+                superuser_username=superuser_username,
+                superuser_password=superuser_password,
+            )
+
+
+class Test_1_11:
+    """
+    Tests for running DC/OS 1.11.
+    """
+
+    def test_oss(
+        self,
+        cluster_backend: ClusterBackend,
+        oss_1_11_artifact: Path,
+    ) -> None:
+        """
+        An open source DC/OS 1.11 cluster can be started.
+        """
+        with Cluster(cluster_backend=cluster_backend) as cluster:
+            cluster.install_dcos_from_path(
+                build_artifact=oss_1_11_artifact,
+                log_output_live=True,
+            )
+            cluster.wait_for_dcos_oss()
+
+    def test_enterprise(
+        self,
+        cluster_backend: ClusterBackend,
+        enterprise_1_11_artifact: Path,
+    ) -> None:
+        """
+        A DC/OS Enterprise 1.11 cluster can be started.
+        """
+        superuser_username = str(uuid.uuid4())
+        superuser_password = str(uuid.uuid4())
+        config = {
+            'superuser_username': superuser_username,
+            'superuser_password_hash': sha512_crypt.hash(superuser_password),
+            'fault_domain_enabled': False,
+            'license_key_contents': license_key_contents,
+        }
+        with Cluster(cluster_backend=cluster_backend) as cluster:
+            cluster.install_dcos_from_path(
+                build_artifact=enterprise_1_11_artifact,
                 extra_config=config,
                 log_output_live=True,
             )
