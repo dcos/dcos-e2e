c0f2acc2504b43d7ca49c4edcb5b51ad53446bdc
Adam Dangoor <adamdangoor@gmail.com>
2018-02-22 19:03:05 +0000

    Script to choose version now works

--- admin/choose_version.py
+++ admin/choose_version.py
@@ -4,11 +4,20 @@ Print a version string for the next version of DC/OS E2E.
 
 import datetime
 
+from dulwich.repo import Repo
+from dulwich.porcelain import tag_list
+
 def get_version():
     utc_now = datetime.datetime.utcnow()
     date_format = '%Y.%m.%d'
     date_str = utc_now.strftime(date_format)
-    micro = 1
+    repo = Repo('.')
+    tag_labels = tag_list(repo)
+    tag_labels = [item.decode() for item in tag_labels]
+    today_tag_labels = [
+        item for item in tag_labels if item.startswith(date_str)
+    ]
+    micro = int(len(today_tag_labels))
     return '{date}.{micro}'.format(date=date_str, micro=micro)
 
 if __name__ == '__main__':
