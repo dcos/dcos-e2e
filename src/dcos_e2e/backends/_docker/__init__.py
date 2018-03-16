b925e1960daa3d3674b60b0ab5a6d5a11af69b6d
Adam Dangoor <adamdangoor@gmail.com>
2018-03-16 17:20:35 +0000

    Use Docker build in helper

--- src/dcos_e2e/backends/_docker/__init__.py
+++ src/dcos_e2e/backends/_docker/__init__.py
@@ -30,72 +30,10 @@ from dcos_e2e.docker_storage_drivers import DockerStorageDriver
 from dcos_e2e.docker_versions import DockerVersion
 from dcos_e2e.node import Node
 
+from ._docker_build import build_docker_image
+
 logging.basicConfig(level=logging.DEBUG)
 LOGGER = logging.getLogger(__name__)
-
-
-def _base_dockerfile(linux_distribution: Distribution) -> Path:
-    """
-    Return the directory including a Dockerfile to use for the base OS image.
-    """
-    dcos_docker_distros = {
-        Distribution.CENTOS_7: 'centos-7',
-        Distribution.UBUNTU_16_04: 'ubuntu-xenial',
-        Distribution.COREOS: 'coreos',
-    }
-
-    distro_path_segment = dcos_docker_distros[linux_distribution]
-    current_file = inspect.stack()[0][1]
-    current_parent = Path(os.path.abspath(current_file)).parent
-    dockerfiles = current_parent / 'resources' / 'dockerfiles' / 'base'
-    return dockerfiles / distro_path_segment
-
-
-def _docker_dockerfile(docker_version: DockerVersion) -> Path:
-    """
-    Return the directory including a Dockerfile to use to install a particular
-    version of Docker.
-    """
-    docker_versions = {
-        DockerVersion.v1_11_2: '1.11.2',
-        DockerVersion.v1_13_1: '1.13.1',
-        DockerVersion.v17_12_1_ce: '17.12.1-ce',
-    }
-
-    version_segment = docker_versions[docker_version]
-    current_file = inspect.stack()[0][1]
-    current_parent = Path(os.path.abspath(current_file)).parent
-    dockerfiles = current_parent / 'resources' / 'dockerfiles' / 'base-docker'
-    return dockerfiles / version_segment
-
-
-def _build_docker_image(
-    tag: str,
-    linux_distribution: Distribution,
-    docker_version: DockerVersion,
-) -> None:
-    """
-    Build a Docker image to use for node containers.
-    """
-    base_tag = tag + ':base'
-
-    client = docker.from_env(version='auto')
-    base_dockerfile = _base_dockerfile(linux_distribution=linux_distribution)
-    docker_dockerfile = _docker_dockerfile(docker_version=docker_version)
-
-    client.images.build(
-        path=str(base_dockerfile),
-        rm=True,
-        forcerm=True,
-        tag=base_tag,
-    )
-
-    client.images.build(
-        path=str(docker_dockerfile),
-        rm=True,
-        forcerm=True,
-        tag=tag,
-    )
 
 
 def _write_key_pair(public_key_path: Path, private_key_path: Path) -> None:
@@ -513,7 +451,7 @@ class DockerCluster(ClusterManager):
         }
 
         docker_image_tag = 'mesosphere/dcos-docker'
-        _build_docker_image(
+        build_docker_image(
             tag=docker_image_tag,
             linux_distribution=cluster_backend.linux_distribution,
             docker_version=cluster_backend.docker_version,
---
+++ src/dcos_e2e/backends/_docker/_docker_build.py
@@ -0,0 +1,67 @@
+"""
+Helpers for building Docker images.
+"""
+
+
+def _base_dockerfile(linux_distribution: Distribution) -> Path:
+    """
+    Return the directory including a Dockerfile to use for the base OS image.
+    """
+    dcos_docker_distros = {
+        Distribution.CENTOS_7: 'centos-7',
+        Distribution.UBUNTU_16_04: 'ubuntu-xenial',
+        Distribution.COREOS: 'coreos',
+    }
+
+    distro_path_segment = dcos_docker_distros[linux_distribution]
+    current_file = inspect.stack()[0][1]
+    current_parent = Path(os.path.abspath(current_file)).parent
+    dockerfiles = current_parent / 'resources' / 'dockerfiles' / 'base'
+    return dockerfiles / distro_path_segment
+
+
+def _docker_dockerfile(docker_version: DockerVersion) -> Path:
+    """
+    Return the directory including a Dockerfile to use to install a particular
+    version of Docker.
+    """
+    docker_versions = {
+        DockerVersion.v1_11_2: '1.11.2',
+        DockerVersion.v1_13_1: '1.13.1',
+        DockerVersion.v17_12_1_ce: '17.12.1-ce',
+    }
+
+    version_segment = docker_versions[docker_version]
+    current_file = inspect.stack()[0][1]
+    current_parent = Path(os.path.abspath(current_file)).parent
+    dockerfiles = current_parent / 'resources' / 'dockerfiles' / 'base-docker'
+    return dockerfiles / version_segment
+
+
+def build_docker_image(
+    tag: str,
+    linux_distribution: Distribution,
+    docker_version: DockerVersion,
+) -> None:
+    """
+    Build a Docker image to use for node containers.
+    """
+    base_tag = tag + ':base'
+
+    client = docker.from_env(version='auto')
+    base_dockerfile = _base_dockerfile(linux_distribution=linux_distribution)
+    docker_dockerfile = _docker_dockerfile(docker_version=docker_version)
+
+    client.images.build(
+        path=str(base_dockerfile),
+        rm=True,
+        forcerm=True,
+        tag=base_tag,
+    )
+
+    client.images.build(
+        path=str(docker_dockerfile),
+        rm=True,
+        forcerm=True,
+        tag=tag,
+    )
