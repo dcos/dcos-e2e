5733a72c057ad62239a59c7df85c53f405e6dff2
Adam Dangoor <adamdangoor@gmail.com>
2018-03-24 11:23:53 +0000

    Stubs for new tests

--- tests/test_cli/test_utils.py
+++ tests/test_cli/test_utils.py
@@ -8,8 +8,24 @@ class TestIsEnterprise:
     Tests for ``is_enterprise``.
     """
 
-    def test_oss(self):
+    def test_oss(
+        self,
+        oss_artifact: Path,
+        oss_1_11_artifact: Path,
+        oss_1_10_artifact: Path,
+    ) -> None:
+        """
+        ``False`` is returned when given a DC/OS OSS artifact.
+        """
         pass
 
-    def test_enterprise(self):
+    def test_enterprise(
+        self,
+        enterprise_artifact: Path,
+        enterprise_1_11_artifact: Path,
+        enterprise_1_10_artifact: Path,
+    ) -> None:
+        """
+        ``True`` is returned when given a DC/OS Enterprise artifact.
+        """
         pass
