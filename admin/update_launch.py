a97ecec852d9e4deeb5943eba24f1a98becae8a9
Adam Dangoor <adamdangoor@gmail.com>
2018-03-27 13:20:52 +0100

    Revert "Progress"
    This reverts commit 88a5452c222f72378340cba828b0baa5a7c63306.

--- admin/update_launch.py
+++ admin/update_launch.py
@@ -7,16 +7,25 @@ python -c 'import dcos_e2e._vendor.dcos_launch'
 import vendorize
 
 
-def test_foo():
-    # package_path = 'src/dcos_e2e/_vendor/dcos_launch'
-    # top_level_names = ['dcos_test_utils']
-    # vendorize._rewrite_imports_in_package(package_path, top_level_names, depth=1)
-    #
-    module_path = 'src/dcos_e2e/_vendor/dcos_launch/util.py'
+def main():
+    package_path = 'src/dcos_e2e/_vendor/dcos_launch'
     top_level_names = ['dcos_test_utils']
-    vendorize._rewrite_imports_in_module(module_path, top_level_names, depth=1)
+    # import pdb; pdb.set_trace()
+    # top_level = vendorize._read_top_level_names(target_directory)
+    # top_level_names = ['dcos_launch', 'dcos_test_utils']
+    import pdb; pdb.set_trace()
+    vendorize._rewrite_imports_in_package(package_path, top_level_names, depth=1)
+    # vendorize.vendorize_requirement(
+    # )
+    # target_directory = 1
+    # top_level_names = 1
+    # vendorize._rewrite_imports(
+    #     target_directory=target_directory,
+    #     top_level_names=top_level_names,
+    # )
+    # pass
 
 
-#
-# if __name__ == '__main__':
-#     main()
+
+if __name__ == '__main__':
+    main()
--- src/dcos_e2e/_vendor/dcos_launch/util.py
+++ src/dcos_e2e/_vendor/dcos_launch/util.py
@@ -9,7 +9,7 @@ import pkg_resources
 from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.asymmetric import rsa
 
-from .. import dcos_test_utils
+import dcos_test_utils
 import yaml
 
 log = logging.getLogger(__name__)
