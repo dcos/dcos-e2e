a84f7ce6d189ab55ae59f67a37b92accbea64b0c
Adam Dangoor <adamdangoor@gmail.com>
2018-03-27 13:21:16 +0100

    Progress

--- admin/update_launch.py
+++ admin/update_launch.py
@@ -8,22 +8,13 @@ import vendorize
 
 
 def main():
-    package_path = 'src/dcos_e2e/_vendor/dcos_launch'
+    # package_path = 'src/dcos_e2e/_vendor/dcos_launch'
+    # top_level_names = ['dcos_test_utils']
+    # vendorize._rewrite_imports_in_package(package_path, top_level_names, depth=1)
+    #
+    module_path = 'src/dcos_e2e/_vendor/dcos_launch/util.py'
     top_level_names = ['dcos_test_utils']
-    # import pdb; pdb.set_trace()
-    # top_level = vendorize._read_top_level_names(target_directory)
-    # top_level_names = ['dcos_launch', 'dcos_test_utils']
-    import pdb; pdb.set_trace()
-    vendorize._rewrite_imports_in_package(package_path, top_level_names, depth=1)
-    # vendorize.vendorize_requirement(
-    # )
-    # target_directory = 1
-    # top_level_names = 1
-    # vendorize._rewrite_imports(
-    #     target_directory=target_directory,
-    #     top_level_names=top_level_names,
-    # )
-    # pass
+    vendorize._rewrite_imports_in_module(module_path, top_level_names, depth=1)
 
 
 
