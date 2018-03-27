c49ca2be742a14dead898b9dcdeb3ed275a5e73c
Adam Dangoor <adamdangoor@gmail.com>
2018-03-27 17:41:47 +0100

    Add tests for 1.9

--- tests/test_dcos_e2e/test_legacy.py
+++ tests/test_dcos_e2e/test_legacy.py
@@ -14,6 +14,54 @@ from dcos_e2e.backends import ClusterBackend
 from dcos_e2e.cluster import Cluster
 
 
+class Test19:
+    """
+    Tests for running DC/OS 1.9.
+    """
+
+    def test_oss(
+        self,
+        cluster_backend: ClusterBackend,
+        oss_1_9_artifact: Path,
+    ) -> None:
+        """
+        An open source DC/OS 1.9 cluster can be started.
+        """
+        with Cluster(cluster_backend=cluster_backend) as cluster:
+            cluster.install_dcos_from_path(
+                build_artifact=oss_1_9_artifact,
+                log_output_live=True,
+            )
+            cluster.wait_for_dcos_oss()
+
+    def test_enterprise(
+        self,
+        cluster_backend: ClusterBackend,
+        enterprise_1_9_artifact: Path,
+    ) -> None:
+        """
+        A DC/OS Enterprise 1.9 cluster can be started.
+        """
+        superuser_username = str(uuid.uuid4())
+        superuser_password = str(uuid.uuid4())
+        config = {
+            'superuser_username': superuser_username,
+            'superuser_password_hash': sha512_crypt.hash(superuser_password),
+            'fault_domain_enabled': False,
+        }
+
+        with Cluster(cluster_backend=cluster_backend) as cluster:
+            cluster.install_dcos_from_path(
+                build_artifact=enterprise_1_9_artifact,
+                extra_config=config,
+                log_output_live=True,
+            )
+            cluster.wait_for_dcos_ee(
+                superuser_username=superuser_username,
+                superuser_password=superuser_password,
+            )
+
+
 class Test110:
     """
     Tests for running DC/OS 1.10.
