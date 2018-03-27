d528bedd4cba1814afbecc463161556734517d56
Adam Dangoor <adamdangoor@gmail.com>
2018-03-27 13:22:26 +0100

    Progress

--- admin/update_launch.py
+++ admin/update_launch.py
@@ -8,13 +8,13 @@ import vendorize
 
 
 def main():
-    # package_path = 'src/dcos_e2e/_vendor/dcos_launch'
-    # top_level_names = ['dcos_test_utils']
-    # vendorize._rewrite_imports_in_package(package_path, top_level_names, depth=1)
-    #
-    module_path = 'src/dcos_e2e/_vendor/dcos_launch/util.py'
+    package_path = 'src/dcos_e2e/_vendor/dcos_launch'
     top_level_names = ['dcos_test_utils']
-    vendorize._rewrite_imports_in_module(module_path, top_level_names, depth=1)
+    vendorize._rewrite_imports_in_package(package_path, top_level_names, depth=1)
+    #
+    # module_path = 'src/dcos_e2e/_vendor/dcos_launch/util.py'
+    # top_level_names = ['dcos_test_utils']
+    # vendorize._rewrite_imports_in_module(module_path, top_level_names, depth=1)
 
 
 
