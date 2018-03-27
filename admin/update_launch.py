d0e805cf77c0206ddd2eb0bf07f5af9fe0e84141
Adam Dangoor <adamdangoor@gmail.com>
2018-03-27 09:45:06 +0100

    Progress on stub

--- admin/update_launch.py
+++ admin/update_launch.py
@@ -1,7 +1,18 @@
 import vendorize
 
+
 def main():
-    pass
+    import pdb; pdb.set_trace()
+    vendorize.vendorize_requirement(
+    )
+    # target_directory = 1
+    # top_level_names = 1
+    # vendorize._rewrite_imports(
+    #     target_directory=target_directory,
+    #     top_level_names=top_level_names,
+    # )
+    # pass
+
 
 if __name__ == '__main__':
     main()
