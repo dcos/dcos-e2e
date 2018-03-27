a40908341b33e4b60b0abac5145a61360bcfc15e
Adam Dangoor <adamdangoor@gmail.com>
2018-03-27 13:49:47 +0100

    Progress

--- admin/update_launch.py
+++ admin/update_launch.py
@@ -1,22 +1,40 @@
-"""
+'''
 Try to get the following to work:
 
 python -c 'import dcos_e2e._vendor.dcos_launch'
-"""
+'''
 
 import vendorize
+import os
+import subprocess
+from pathlib import Path
 
 
 def test_main():
-    package_path = 'src/dcos_e2e/_vendor/dcos_launch'
-    top_level_names = ['dcos_test_utils']
-    vendorize._rewrite_imports_in_package(package_path, top_level_names, depth=1)
+    # package_path = 'src/dcos_e2e/_vendor/dcos_launch'
+    # top_level_names = ['dcos_test_utils']
+    # vendorize._rewrite_imports_in_package(package_path, top_level_names, depth=1)
     #
+    launch_url = 'git+https://github.com/dcos/dcos-launch@fad6d70caf282f7260d2d7af88f044ccfd64f8c7',
+    test_utils_url = 'git+https://github.com/dcos/dcos-test-utils@a4cd8815fde6624a645c83eef85abde88b73a38f'
+    target_directory = 'src/dcos_e2e/_vendor'
+    if not os.path.exists(target_directory):
+        os.makedirs(target_directory)
+
+    init_file = Path(target_directory) / '__init__.py'
+    init_file.touch()
+
+    for requirement in [launch_url, test_utils_url]:
+        subprocess.check_call(
+            ['pip', 'install', '--no-dependencies', '--target', target_directory, requirement])
+        top_level_names = ['dcos_launch', 'dcos_test_utils']
+        vendorize._rewrite_imports(target_directory, top_level_names)
+
     # module_path = 'src/dcos_e2e/_vendor/dcos_launch/util.py'
     # top_level_names = ['dcos_test_utils']
     # vendorize._rewrite_imports_in_module(module_path, top_level_names, depth=1)
 
-
-
-if __name__ == '__main__':
-    main()
+#
+#
+# if __name__ == '__main__':
+#     main()
--- src/dcos_e2e/_vendor/dcos_launch/ip-detect-public/gcp.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# Example ip-detect script using an external authority
-# Uses the GCE metadata server to get the node's external
-# ipv4 address
-
-/opt/mesosphere/bin/curl -fsSL -H "Metadata-Flavor: Google" http://169.254.169.254/computeMetadata/v1/instance/network-interfaces/0/access-configs/0/external-ip
