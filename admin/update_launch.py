9ad91d271a0228896b6e2919292cd4bb3cedc14d
Adam Dangoor <adamdangoor@gmail.com>
2018-03-27 14:47:56 +0100

    Progress

--- admin/update_launch.py
+++ admin/update_launch.py
@@ -12,7 +12,8 @@ def main() -> None:
         'dcos_launch': 'git+https://github.com/dcos/dcos-launch@fad6d70caf282f7260d2d7af88f044ccfd64f8c7',  # noqa: E501
         'dcos_test_utils': 'git+https://github.com/dcos/dcos-test-utils@a4cd8815fde6624a645c83eef85abde88b73a38f',  # noqa: E501
     }
-    target_directory = 'src/dcos_e2e/_vendor'
+    target_directory = Path('src/dcos_e2e/_vendor')
+    target_directory.mkdir(exist_ok=False)
     init_file = Path(target_directory) / '__init__.py'
     Path(init_file).touch()
 
@@ -23,19 +24,17 @@ def main() -> None:
                 'install',
                 '--no-dependencies',
                 '--target',
-                target_directory,
+                str(target_directory),
                 requirement,
             ],
         )
-
-    top_level_names = ['dcos_launch', 'dcos_test_utils']
 
     # Ideally we would not use a protected function, however, this trade-off
     # has been considered - we want to use the vendored ``dcos_test_utils``
     # from the vendored ``dcos_launch``.
     vendorize._rewrite_imports(  # pylint: disable=protected-access
         target_directory=target_directory,
-        top_level_names=top_level_names,
+        top_level_names=list(package_name_to_url.keys()),
     )
 
 
