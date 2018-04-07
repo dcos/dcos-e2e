a60179c5dfdf5100f80ad1564006becc608b9dde
Adam Dangoor <adamdangoor@gmail.com>
2018-04-07 11:47:45 +0200

    Separate tests for Ubuntu

--- tests/test_dcos_e2e/backends/docker/test_distributions.py
+++ tests/test_dcos_e2e/backends/docker/test_distributions.py
@@ -184,7 +184,7 @@ class TestUbuntu1604:
     Tests for the Ubuntu 16.04 distribution option.
     """
 
-    def test_oss(
+    def test_oss_ubuntu(
         self,
         oss_artifact: Path,
     ) -> None:
