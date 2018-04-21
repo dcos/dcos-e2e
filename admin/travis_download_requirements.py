aefc58990f51dec47c2935a963815771c5ac6f67
Adam Dangoor <adamdangoor@gmail.com>
2018-04-21 16:02:37 +0100

    Add docstring

---
+++ admin/travis_download_requirements.py
@@ -0,0 +1,7 @@
+"""
+Download requirements for a test pattern.
+
+This separates the download step from the test step. We could download all
+artifacts for all tests, but in the interest of speed, we only download what we
+need.
+"""
