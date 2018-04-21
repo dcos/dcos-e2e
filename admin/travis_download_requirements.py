ae53b9e6ca22dbf88f717ff5b631eb08632708ce
Adam Dangoor <adamdangoor@gmail.com>
2018-04-21 16:43:11 +0100

    Switch downloading artifacts to a Python script

--- admin/travis_download_requirements.py
+++ admin/travis_download_requirements.py
@@ -6,17 +6,63 @@ artifacts for all tests, but in the interest of speed, we only download what we
 need.
 """
 
-OSS_MASTER_ARTIFACT_URL = 'https://downloads.dcos.io/dcos/testing/master/dcos_generate_config.sh'
-OSS_1_9_ARTIFACT_URL = 'https://downloads.dcos.io/dcos/testing/1.9/dcos_generate_config.sh'
-OSS_1_10_ARTIFACT_URL = 'https://downloads.dcos.io/dcos/testing/1.10/dcos_generate_config.sh'
-OSS_1_11_ARTIFACT_URL = 'https://downloads.dcos.io/dcos/testing/1.11/dcos_generate_config.sh'
+import os
+from pathlib import Path
 
-OSS_MASTER_ARTIFACT_PATH = '/tmp/dcos_generate_config.sh'
-OSS_1_9_ARTIFACT_PATH = '/tmp/dcos_generate_config_1_9.sh'
-OSS_1_10_ARTIFACT_PATH = '/tmp/dcos_generate_config_1_10.sh'
-OSS_1_11_ARTIFACT_PATH = '/tmp/dcos_generate_config_1_11.sh'
+import requests
 
-EE_MASTER_ARTIFACT_PATH = '/tmp/dcos_generate_config.ee.sh'
-EE_1_9_ARTIFACT_PATH = '/tmp/dcos_generate_config_1_9.ee.sh'
-EE_1_10_ARTIFACT_PATH = '/tmp/dcos_generate_config_1_10.ee.sh'
-EE_1_11_ARTIFACT_PATH = '/tmp/dcos_generate_config_1_11.ee.sh'
+OSS_PATTERN = (
+    'https://downloads.dcos.io/dcos/testing/{version}/dcos_generate_config.sh'
+)
+OSS_MASTER_ARTIFACT_URL = OSS_PATTERN.format(version='master')
+OSS_1_9_ARTIFACT_URL = OSS_PATTERN.format(version='1.9')
+OSS_1_10_ARTIFACT_URL = OSS_PATTERN.format(version='1.10')
+OSS_1_11_ARTIFACT_URL = OSS_PATTERN.format(version='1.11')
+
+EE_MASTER_ARTIFACT_URL = os.environ['EE_MASTER_ARTIFACT_URL']
+EE_1_9_ARTIFACT_URL = os.environ['EE_1_9_ARTIFACT_URL']
+EE_1_10_ARTIFACT_URL = os.environ['EE_1_10_ARTIFACT_URL']
+EE_1_11_ARTIFACT_URL = os.environ['EE_1_11_ARTIFACT_URL']
+
+OSS_MASTER_ARTIFACT_PATH = Path('/tmp/dcos_generate_config.sh')
+OSS_1_9_ARTIFACT_PATH = Path('/tmp/dcos_generate_config_1_9.sh')
+OSS_1_10_ARTIFACT_PATH = Path('/tmp/dcos_generate_config_1_10.sh')
+OSS_1_11_ARTIFACT_PATH = Path('/tmp/dcos_generate_config_1_11.sh')
+
+EE_MASTER_ARTIFACT_PATH = Path('/tmp/dcos_generate_config.ee.sh')
+EE_1_9_ARTIFACT_PATH = Path('/tmp/dcos_generate_config_1_9.ee.sh')
+EE_1_10_ARTIFACT_PATH = Path('/tmp/dcos_generate_config_1_10.ee.sh')
+EE_1_11_ARTIFACT_PATH = Path('/tmp/dcos_generate_config_1_11.ee.sh')
+
+
+def download_file(url: str, path: Path):
+    """
+    Download a file to a given path.
+    """
+    stream = requests.get(url, stream=True)
+    with open(str(path), 'wb') as fd:
+        for chunk in stream.iter_content():
+            fd.write(chunk)
+
+
+def main():
+    """
+    Download artifacts.
+    """
+    downloads = (
+        (OSS_MASTER_ARTIFACT_URL, OSS_MASTER_ARTIFACT_PATH),
+        (OSS_1_9_ARTIFACT_URL, OSS_1_9_ARTIFACT_PATH),
+        (OSS_1_10_ARTIFACT_URL, OSS_1_10_ARTIFACT_PATH),
+        (OSS_1_11_ARTIFACT_URL, OSS_1_11_ARTIFACT_PATH),
+        (EE_MASTER_ARTIFACT_URL, EE_MASTER_ARTIFACT_PATH),
+        (EE_1_9_ARTIFACT_URL, EE_1_9_ARTIFACT_PATH),
+        (EE_1_10_ARTIFACT_URL, EE_1_10_ARTIFACT_PATH),
+        (EE_1_11_ARTIFACT_URL, EE_1_11_ARTIFACT_PATH),
+    )
+
+    for url, path in downloads:
+        download_file(url=url, path=path)
+
+
+if __name__ == '__main__':
+    main()
