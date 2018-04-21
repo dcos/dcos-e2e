f09ddfb3e879f6bbe6af388ba1a591321df79324
Adam Dangoor <adamdangoor@gmail.com>
2018-04-21 16:04:24 +0100

    Start fleshing out download script

--- admin/travis_download_requirements.py
+++ admin/travis_download_requirements.py
@@ -5,3 +5,18 @@ This separates the download step from the test step. We could download all
 artifacts for all tests, but in the interest of speed, we only download what we
 need.
 """
+
+OSS_MASTER_ARTIFACT_URL = 'https://downloads.dcos.io/dcos/testing/master/dcos_generate_config.sh'
+OSS_1_9_ARTIFACT_URL = 'https://downloads.dcos.io/dcos/testing/1.9/dcos_generate_config.sh'
+OSS_1_10_ARTIFACT_URL = 'https://downloads.dcos.io/dcos/testing/1.10/dcos_generate_config.sh'
+OSS_1_11_ARTIFACT_URL = 'https://downloads.dcos.io/dcos/testing/1.11/dcos_generate_config.sh'
+
+OSS_MASTER_ARTIFACT_PATH = '/tmp/dcos_generate_config.sh'
+OSS_1_9_ARTIFACT_PATH = '/tmp/dcos_generate_config_1_9.sh'
+OSS_1_10_ARTIFACT_PATH = '/tmp/dcos_generate_config_1_10.sh'
+OSS_1_11_ARTIFACT_PATH = '/tmp/dcos_generate_config_1_11.sh'
+
+EE_MASTER_ARTIFACT_PATH = '/tmp/dcos_generate_config.ee.sh'
+EE_1_9_ARTIFACT_PATH = '/tmp/dcos_generate_config_1_9.ee.sh'
+EE_1_10_ARTIFACT_PATH = '/tmp/dcos_generate_config_1_10.ee.sh'
+EE_1_11_ARTIFACT_PATH = '/tmp/dcos_generate_config_1_11.ee.sh'
