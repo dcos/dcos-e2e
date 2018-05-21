9e5badb2f1800d2f69e90e93c6cb96800fcf593a
Adam Dangoor <adamdangoor@gmail.com>
2018-05-21 16:48:09 +0100

    Update more tests

--- tests/test_dcos_e2e/test_cluster.py
+++ tests/test_dcos_e2e/test_cluster.py
@@ -36,6 +36,7 @@ class TestIntegrationTests:
         """
         with Cluster(cluster_backend=cluster_backend) as dcos_cluster:
             dcos_cluster.install_dcos_from_path(
+                dcos_config=cluster.base_config,
                 build_artifact=oss_artifact,
                 log_output_live=True,
             )
@@ -181,7 +182,8 @@ class TestInstallDcosFromPathLogging:
                 cluster_backend=cluster_backend,
             ) as cluster:
                 cluster.install_dcos_from_path(
-                    oss_artifact,
+                    build_artifact=oss_artifact,
+                    dcos_config=cluster.base_config,
                     log_output_live=True,
                 )
 
