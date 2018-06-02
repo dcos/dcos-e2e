75bf39110a0f94fe58657bf9e41d309c1d2474d9
Adam Dangoor <adamdangoor@gmail.com>
2018-06-02 11:08:45 +0100

    Start simplifying Docker tooling

--- src/dcos_e2e/_node_transports/__init__.py
+++ src/dcos_e2e/_node_transports/__init__.py
@@ -5,8 +5,6 @@ Classes to communicate with nodes.
 from ._docker_exec_transport import DockerExecTransport
 from ._ssh_transport import SSHTransport
 
-
-
 __all__ = [
     'SSHTransport',
     'DockerExecTransport',
--- src/dcos_e2e/_node_transports/_docker_exec_transport.py
+++ src/dcos_e2e/_node_transports/_docker_exec_transport.py
@@ -4,6 +4,8 @@ Utilities to connect to nodes with Docker exec.
 
 import io
 import subprocess
+# tools.py
+import sys
 import tarfile
 from ipaddress import IPv4Address
 from pathlib import Path
@@ -16,60 +18,70 @@ from dcos_e2e._node_transports._base_classes import NodeTransport
 
 LOGGER = get_logger(__name__)
 
-# tools.py
-import sys
 
 
-def container_exec(container, cmd, stdout=True, stderr=True, stdin=False,
-                   tty=False, privileged=False, user='', detach=False,
-                   stream=False, socket=False, environment=None, workdir=None):
-  """
+def container_exec(
+    container,
+    cmd,
+    tty=False,
+    user='',
+    detach=False,
+    stream=False,
+    socket=False,
+    environment=None,
+    workdir=None
+):
+    """
   An enhanced version of #docker.Container.exec_run() which returns an object
   that can be properly inspected for the status of the executed commands.
   """
 
-  exec_id = container.client.api.exec_create(
-    container.id, cmd, stdout=stdout, stderr=stderr, stdin=stdin, tty=tty,
-    privileged=privileged, user=user, environment=environment,
-    workdir=workdir)['Id']
+    exec_id = container.client.api.exec_create(
+        container.id,
+        cmd,
+        tty=tty,
+        user=user,
+        environment=environment,
+        workdir=workdir,
+    )['Id']
 
-  output = container.client.api.exec_start(
-    exec_id, detach=detach, tty=tty, stream=stream, socket=socket)
+    output = container.client.api.exec_start(
+        exec_id, detach=detach, tty=tty, stream=stream, socket=socket
+    )
 
-  return ContainerExec(container.client, exec_id, output)
+    return ContainerExec(container.client, exec_id, output)
 
 
 class ContainerExec(object):
+    def __init__(self, client, id, output):
+        self.client = client
+        self.id = id
+        self.output = output
 
-  def __init__(self, client, id, output):
-    self.client = client
-    self.id = id
-    self.output = output
+    def inspect(self):
+        return self.client.api.exec_inspect(self.id)
 
-  def inspect(self):
-    return self.client.api.exec_inspect(self.id)
+    def poll(self):
+        return self.inspect()['ExitCode']
 
-  def poll(self):
-    return self.inspect()['ExitCode']
-
-  def communicate(self, line_prefix=b''):
-    for data in self.output:
-      if not data: continue
-      offset = 0
-      while offset < len(data):
-        sys.stdout.buffer.write(line_prefix)
-        nl = data.find(b'\n', offset)
-        if nl >= 0:
-          slice = data[offset:nl+1]
-          offset = nl+1
-        else:
-          slice = data[offset:]
-          offset += len(slice)
-        sys.stdout.buffer.write(slice)
-      sys.stdout.flush()
-    while self.poll() is None:
-      raise RuntimeError('Hm could that really happen?')
-    return self.poll()
+    def communicate(self, line_prefix=b''):
+        for data in self.output:
+            if not data: continue
+            offset = 0
+            while offset < len(data):
+                sys.stdout.buffer.write(line_prefix)
+                nl = data.find(b'\n', offset)
+                if nl >= 0:
+                    slice = data[offset:nl + 1]
+                    offset = nl + 1
+                else:
+                    slice = data[offset:]
+                    offset += len(slice)
+                sys.stdout.buffer.write(slice)
+            sys.stdout.flush()
+        while self.poll() is None:
+            raise RuntimeError('Hm could that really happen?')
+        return self.poll()
 
 
 class DockerExecTransport(NodeTransport):
@@ -163,7 +175,6 @@ class DockerExecTransport(NodeTransport):
             stderr=stderr,
         )
 
-
     def popen(
         self,
         args: List[str],
@@ -187,13 +198,7 @@ class DockerExecTransport(NodeTransport):
         Returns:
             The pipe object attached to the specified process.
         """
-        client = docker.from_env(version='auto')
-        containers = client.containers.list()
-        [container] = [
-            container for container in containers
-            if container.attrs['NetworkSettings']['IPAddress'] ==
-            str(public_ip_address)
-        ]
+        raise NotImplementedError
 
     def send_file(
         self,
