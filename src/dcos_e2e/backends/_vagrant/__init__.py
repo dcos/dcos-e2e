0a76e24dcd025ea673b98407cd53e9f5e02c03c9
Adam Dangoor <adamdangoor@gmail.com>
2018-06-07 14:50:40 +0100

    Progress towards agents and public agents

--- src/dcos_e2e/backends/_vagrant/__init__.py
+++ src/dcos_e2e/backends/_vagrant/__init__.py
@@ -6,6 +6,8 @@ from ipaddress import IPv4Address
 from pathlib import Path
 from typing import Any, Dict, List, Set, Tuple, Type
 
+import vagrant
+
 from .._base_classes import ClusterBackend, ClusterManager
 from dcos_e2e.node import Node, Transport
 from dcos_e2e._common import run_subprocess
@@ -125,30 +127,30 @@ class VagrantCluster(ClusterManager):
         """
         Destroy all nodes in the cluster.
         """
+        dcos_vagrant_path = Path(__file__).parent / 'resources' / 'dcos-vagrant'
+        client = vagrant.Vagrant(root=str(dcos_vagrant_path))
 
     @property
     def masters(self) -> Set[Node]:
         """
         Return all DC/OS master :class:`.node.Node` s.
         """
-        import vagrant
-
         dcos_vagrant_path = Path(__file__).parent / 'resources' / 'dcos-vagrant'
         client = vagrant.Vagrant(root=str(dcos_vagrant_path))
-        vagrant_internal_path = dcos_vagrant_path / '.vagrant' / 'dcos'
-        ip_address = IPv4Address('192.168.65.90')
 
-        ssh_key_path = Path(client.keyfile())
+        ip_addresses = [IPv4Address('192.168.65.90')]
+
         nodes = set([])
-        nodes.add(
-            Node(
-                public_ip_address=ip_address,
-                private_ip_address=ip_address,
-                default_user='vagrant',
-                ssh_key_path=ssh_key_path,
-                default_transport=self._default_transport,
-            ),
-        )
+        for ip_address in ip_addresses:
+            nodes.add(
+                Node(
+                    public_ip_address=ip_address,
+                    private_ip_address=ip_address,
+                    default_user='vagrant',
+                    ssh_key_path=Path(client.keyfile()),
+                    default_transport=self._default_transport,
+                ),
+            )
 
         return nodes
 
@@ -157,14 +159,48 @@ class VagrantCluster(ClusterManager):
         """
         Return all DC/OS agent :class:`.node.Node` s.
         """
-        return set([])
+        dcos_vagrant_path = Path(__file__).parent / 'resources' / 'dcos-vagrant'
+        client = vagrant.Vagrant(root=str(dcos_vagrant_path))
+
+        ip_addresses = [IPv4Address('192.168.65.111')]
+
+        nodes = set([])
+        for ip_address in ip_addresses:
+            nodes.add(
+                Node(
+                    public_ip_address=ip_address,
+                    private_ip_address=ip_address,
+                    default_user='vagrant',
+                    ssh_key_path=Path(client.keyfile()),
+                    default_transport=self._default_transport,
+                ),
+            )
+
+        return nodes
 
     @property
     def public_agents(self) -> Set[Node]:
         """
         Return all DC/OS public agent :class:`.node.Node` s.
         """
-        return set([])
+        dcos_vagrant_path = Path(__file__).parent / 'resources' / 'dcos-vagrant'
+        client = vagrant.Vagrant(root=str(dcos_vagrant_path))
+
+        ip_addresses = [IPv4Address('192.168.65.60')]
+
+        nodes = set([])
+        for ip_address in ip_addresses:
+            nodes.add(
+                Node(
+                    public_ip_address=ip_address,
+                    private_ip_address=ip_address,
+                    default_user='vagrant',
+                    ssh_key_path=Path(client.keyfile()),
+                    default_transport=self._default_transport,
+                ),
+            )
+
+        return nodes
 
     @property
     def base_config(self) -> Dict[str, Any]:
