384c1b00d7d474a5081326c84a382b3d20206fbc
Adam Dangoor <adamdangoor@gmail.com>
2018-06-15 12:04:07 +0100

    Move things around to narrow diff

--- src/cli/__init__.py
+++ src/cli/__init__.py
@@ -76,47 +76,6 @@ from ._validators import (
 from .commands.mac_network import destroy_mac_network, setup_mac_network
 
 
-def _set_logging(
-    ctx: click.core.Context,
-    param: Union[click.core.Option, click.core.Parameter],
-    value: Optional[Union[int, bool, str]],
-) -> None:
-    """
-    Set logging level depending on the chosen verbosity.
-    """
-    # We "use" variables to satisfy linting tools.
-    for _ in (ctx, param):
-        pass
-
-    value = min(value, 3)
-    value = max(value, 0)
-    verbosity_map = {
-        0: logging.WARNING,
-        1: logging.INFO,
-        2: logging.DEBUG,
-        3: logging.NOTSET,
-    }
-    # Disable logging calls of the given severity level or below.
-    logging.disable(verbosity_map[int(value or 0)])
-
-
-@click.option(
-    '-v',
-    '--verbose',
-    count=True,
-    callback=_set_logging,
-)
-@click.group(name='dcos-docker')
-@click.version_option()
-def dcos_docker(verbose: None) -> None:
-    """
-    Manage DC/OS clusters on Docker.
-    """
-    # We "use" variables to satisfy linting tools.
-    for _ in (verbose, ):
-        pass
-
-
 def _existing_cluster_id_option(command: Callable[..., None],
                                 ) -> Callable[..., None]:
     """
@@ -185,6 +144,47 @@ def _write_key_pair(public_key_path: Path, private_key_path: Path) -> None:
 
     public_key_path.write_bytes(data=public_key)
     private_key_path.write_bytes(data=private_key)
+
+
+def _set_logging(
+    ctx: click.core.Context,
+    param: Union[click.core.Option, click.core.Parameter],
+    value: Optional[Union[int, bool, str]],
+) -> None:
+    """
+    Set logging level depending on the chosen verbosity.
+    """
+    # We "use" variables to satisfy linting tools.
+    for _ in (ctx, param):
+        pass
+
+    value = min(value, 3)
+    value = max(value, 0)
+    verbosity_map = {
+        0: logging.WARNING,
+        1: logging.INFO,
+        2: logging.DEBUG,
+        3: logging.NOTSET,
+    }
+    # Disable logging calls of the given severity level or below.
+    logging.disable(verbosity_map[int(value or 0)])
+
+
+@click.option(
+    '-v',
+    '--verbose',
+    count=True,
+    callback=_set_logging,
+)
+@click.group(name='dcos-docker')
+@click.version_option()
+def dcos_docker(verbose: None) -> None:
+    """
+    Manage DC/OS clusters on Docker.
+    """
+    # We "use" variables to satisfy linting tools.
+    for _ in (verbose, ):
+        pass
 
 
 @dcos_docker.command('create')
