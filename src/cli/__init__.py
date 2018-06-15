1fa5834bef5288f255388b9cea67359f6284d29c
Adam Dangoor <adamdangoor@gmail.com>
2018-06-15 13:58:26 +0100

    Start of moving destroy commands around

--- src/cli/__init__.py
+++ src/cli/__init__.py
@@ -10,7 +10,6 @@ import tarfile
 import tempfile
 import uuid
 from pathlib import Path
-from shutil import rmtree
 from subprocess import CalledProcessError
 from typing import Any, Callable, Dict, List, Optional, Tuple, Union
 
@@ -514,63 +513,6 @@ def create(
         'to become ready.'
     ).format(cluster_id=cluster_id)
     click.echo(started_message, err=True)
-
-
-@dcos_docker.command('destroy-list')
-@click.argument(
-    'cluster_ids',
-    nargs=-1,
-    type=str,
-)
-@node_transport_option
-@click.pass_context
-def destroy_list(
-    ctx: click.core.Context,
-    cluster_ids: List[str],
-    transport: Transport,
-) -> None:
-    """
-    Destroy clusters.
-
-    To destroy all clusters, run ``dcos-docker destroy $(dcos-docker list)``.
-    """
-    for cluster_id in cluster_ids:
-        if cluster_id not in existing_cluster_ids():
-            warning = 'Cluster "{cluster_id}" does not exist'.format(
-                cluster_id=cluster_id,
-            )
-            click.echo(warning, err=True)
-            continue
-
-        ctx.invoke(
-            destroy,
-            cluster_id=cluster_id,
-            transport=transport,
-        )
-
-
-@dcos_docker.command('destroy')
-@existing_cluster_id_option
-@node_transport_option
-def destroy(cluster_id: str, transport: Transport) -> None:
-    """
-    Destroy a cluster.
-    """
-    with click_spinner.spinner():
-        cluster_containers = ClusterContainers(
-            cluster_id=cluster_id,
-            transport=transport,
-        )
-        containers = {
-            *cluster_containers.masters,
-            *cluster_containers.agents,
-            *cluster_containers.public_agents,
-        }
-        rmtree(path=str(cluster_containers.workspace_dir), ignore_errors=True)
-        for container in containers:
-            container.stop()
-            container.remove(v=True)
-    click.echo(cluster_id)
 
 
 @dcos_docker.command('wait')
