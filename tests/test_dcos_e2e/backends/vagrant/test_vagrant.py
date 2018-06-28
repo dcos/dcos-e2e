13e2616bf9a6ab2de921081ccb97ceb61860d4a1
Adam Dangoor <adamdangoor@gmail.com>
2018-06-28 22:40:43 +0100

    Test only OSS

--- tests/test_dcos_e2e/backends/vagrant/test_vagrant.py
+++ tests/test_dcos_e2e/backends/vagrant/test_vagrant.py
@@ -18,40 +18,26 @@ class TestRunIntegrationTest:
     Tests for functionality specific to the Vagrant backend.
     """
 
-    def test_run_enterprise_integration_test(
+    def test_run_integration_test(
         self,
-        enterprise_artifact: Path,
         oss_artifact: Path,
-        license_key_contents: str,
     ) -> None:
         """
         It is possible to run DC/OS integration tests on Vagrant.
         This test module only requires a single master node.
         """
-        superuser_username = 'admin'
-        superuser_password = 'admin'
-
-        config = {
-            'superuser_username': superuser_username,
-            'superuser_password_hash': sha512_crypt.hash(superuser_password),
-            'fault_domain_enabled': False,
-            'license_key_contents': license_key_contents,
-            'security': 'strict',
-        }
-
         config = {}
 
         with Cluster(
             cluster_backend=Vagrant(),
             masters=1,
+            agents=0,
+            public_agents=0,
         ) as cluster:
 
             cluster.install_dcos_from_path(
                 build_artifact=oss_artifact,
-                dcos_config={
-                    **cluster.base_config,
-                    **config,
-                },
+                dcos_config=cluster.base_config,
                 log_output_live=True,
             )
 
@@ -60,9 +46,5 @@ class TestRunIntegrationTest:
             # No error is raised with a successful command.
             cluster.run_integration_tests(
                 pytest_command=['pytest', '-vvv', '-s', '-x', 'test_units.py'],
-                env={
-                    'DCOS_LOGIN_UNAME': superuser_username,
-                    'DCOS_LOGIN_PW': superuser_password,
-                },
                 log_output_live=True,
             )
