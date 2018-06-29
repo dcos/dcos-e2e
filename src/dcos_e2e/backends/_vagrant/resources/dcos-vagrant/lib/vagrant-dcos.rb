3143f4b3c83a61090d917c5c9781b7b5dc2fdb8d
Adam Dangoor <adamdangoor@gmail.com>
2018-06-29 17:23:59 +0100

    Remove DC/OS Vagrant [skip ci]

--- src/dcos_e2e/backends/_vagrant/resources/dcos-vagrant/lib/vagrant-dcos.rb
+++
@@ -1,9 +0,0 @@
-require_relative 'vagrant-dcos/plugin'
-
-module VagrantPlugins
-  module DCOS
-    def self.source_root
-      @source_root ||= Pathname.new(File.expand_path('../../', __FILE__))
-    end
-  end
-end
