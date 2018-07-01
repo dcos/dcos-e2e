7d1b4068cf22628d5746abcf39a8f0bb87c7a454
Adam Dangoor <adamdangoor@gmail.com>
2018-07-02 00:52:15 +0100

    Add dcos vagrant stub CLI [skip ci]

--- src/cli/__init__.py
+++ src/cli/__init__.py
@@ -3,7 +3,9 @@ Top level CLI commands.
 """
 
 from .dcos_docker import dcos_docker
+from .dcos_vagrant import dcos_vagrant
 
 __all__ = [
     'dcos_docker',
+    'dcos_vagrant',
 ]
---
+++ src/cli/dcos_vagrant/__init__.py
@@ -0,0 +1,49 @@
+"""
+A CLI for controlling DC/OS clusters on Vagrant.
+"""
+
+import logging
+from typing import Optional, Union
+
+import click
+
+
+def _set_logging(
+    ctx: click.core.Context,
+    param: Union[click.core.Option, click.core.Parameter],
+    value: Optional[Union[int, bool, str]],
+) -> None:
+    """
+    Set logging level depending on the chosen verbosity.
+    """
+    # We "use" variables to satisfy linting tools.
+    for _ in (ctx, param):
+        pass
+
+    value = min(value, 3)
+    value = max(value, 0)
+    verbosity_map = {
+        0: logging.WARNING,
+        1: logging.INFO,
+        2: logging.DEBUG,
+        3: logging.NOTSET,
+    }
+    # Disable logging calls of the given severity level or below.
+    logging.disable(verbosity_map[int(value or 0)])
+
+
+@click.option(
+    '-v',
+    '--verbose',
+    count=True,
+    callback=_set_logging,
+)
+@click.group(name='dcos-vagrant')
+@click.version_option()
+def dcos_vagrant(verbose: None) -> None:
+    """
+    Manage DC/OS clusters on Vagrant.
+    """
+    # We "use" variables to satisfy linting tools.
+    for _ in (verbose, ):
+        pass
