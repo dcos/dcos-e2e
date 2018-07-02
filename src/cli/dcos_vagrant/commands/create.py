b173c9c2ee12dc29e3ffcd35a1b7fc146a4e3c76
Adam Dangoor <adamdangoor@gmail.com>
2018-07-02 18:51:58 +0100

    Progress towards fixing lint [skip ci]

--- src/cli/dcos_vagrant/commands/create.py
+++ src/cli/dcos_vagrant/commands/create.py
@@ -3,9 +3,11 @@ Tools for creating a DC/OS cluster.
 """
 
 import sys
+from pathlib import Path
 from subprocess import CalledProcessError
 
 import click
+import click_spinner
 
 from dcos_e2e.backends import Vagrant
 from dcos_e2e.cluster import Cluster
@@ -13,27 +15,40 @@ from dcos_e2e.cluster import Cluster
 
 @click.command('create')
 @click.argument('artifact', type=click.Path(exists=True))
-@click.pass_context
-def create(
-    ctx: click.core.Context,
-    artifact: str,
-) -> None:
+def create(artifact: str) -> None:
     """
-    Create a DC/OS cluster.
+    Create an OSS DC/OS cluster.
     """
     cluster_backend = Vagrant()
-    files_to_copy_to_installer = []
     masters = 1
     agents = 1
     public_agents = 1
+    extra_config = {}
+
+    artifact_path = Path(artifact).resolve()
+
     try:
-        Cluster(
+        cluster = Cluster(
             cluster_backend=cluster_backend,
             masters=masters,
             agents=agents,
             public_agents=public_agents,
-            files_to_copy_to_installer=files_to_copy_to_installer,
+            files_to_copy_to_installer=[],
         )
     except CalledProcessError as exc:
         click.echo('Error creating cluster.', err=True)
+        sys.exit(exc.returncode)
+
+    try:
+        with click_spinner.spinner():
+            cluster.install_dcos_from_path(
+                build_artifact=artifact_path,
+                dcos_config={
+                    **cluster.base_config,
+                    **extra_config,
+                },
+            )
+    except CalledProcessError as exc:
+        click.echo('Error installing DC/OS.', err=True)
+        cluster.destroy()
         sys.exit(exc.returncode)
