dee1208426c297574763c10fab76d6de4c2ca76a
Adam Dangoor <adamdangoor@gmail.com>
2018-07-02 18:46:36 +0100

    Fix some lint issues [skip ci]

--- src/cli/dcos_vagrant/commands/create.py
+++ src/cli/dcos_vagrant/commands/create.py
@@ -2,10 +2,14 @@
 Tools for creating a DC/OS cluster.
 """
 
-from pathlib import Path
-from typing import Any, Dict, Optional
+import sys
+from subprocess import CalledProcessError
 
 import click
+
+from dcos_e2e.backends import Vagrant
+from dcos_e2e.cluster import Cluster
+
 
 @click.command('create')
 @click.argument('artifact', type=click.Path(exists=True))
@@ -16,5 +20,20 @@ def create(
 ) -> None:
     """
     Create a DC/OS cluster.
-    """  # noqa: E501
-    pass
+    """
+    cluster_backend = Vagrant()
+    files_to_copy_to_installer = []
+    masters = 1
+    agents = 1
+    public_agents = 1
+    try:
+        Cluster(
+            cluster_backend=cluster_backend,
+            masters=masters,
+            agents=agents,
+            public_agents=public_agents,
+            files_to_copy_to_installer=files_to_copy_to_installer,
+        )
+    except CalledProcessError as exc:
+        click.echo('Error creating cluster.', err=True)
+        sys.exit(exc.returncode)
