06d1962e35f88d1a41028afad2d5964d4ce1a304
Adam Dangoor <adamdangoor@gmail.com>
2018-07-06 00:23:28 +0100

    Add sed check to Vagrant doctor [skip ci]

--- src/cli/dcos_vagrant/commands/doctor.py
+++ src/cli/dcos_vagrant/commands/doctor.py
@@ -2,7 +2,11 @@
 Checks for showing up common sources of errors with the Vagrant backend.
 """
 
+import sys
+
 import click
+
+from cli.common.doctor import CheckLevels, check_1_9_sed
 
 
 @click.command('doctor')
@@ -10,3 +14,11 @@ def doctor() -> None:
     """
     Diagnose common issues which stop DC/OS E2E from working correctly.
     """
+    check_functions = [
+        check_1_9_sed,
+    ]
+
+    highest_level = max(function() for function in check_functions)
+
+    if highest_level == CheckLevels.ERROR:
+        sys.exit(1)
