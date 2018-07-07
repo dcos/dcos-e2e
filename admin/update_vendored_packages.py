07b86a55882247a17da9ce747085ffce3906ecc1
Adam Dangoor <adamdangoor@gmail.com>
2018-07-07 23:12:32 +0100

    Start of refactor to simplify adding vendored requirements

--- admin/update_vendored_packages.py
+++ admin/update_vendored_packages.py
@@ -16,29 +16,25 @@ def main() -> None:
     We use our own script as we want the vendored ``dcos_launch`` to use the
     vendored ``dcos_test_utils``.
     """
-    vendored_launch_sha = '09c1d53652d8e91aad5f1c246ef24646de2eb4c1'
-    vendored_test_utils_sha = '00f1a62ef673ebc34e29d9db488dd06b0c1ae4ec'
-
-    launch = 'git+https://github.com/dcos/dcos-launch@{sha}'.format(
-        sha=vendored_launch_sha,
+    launch = (
+        'dcos_launch',
+        'git+https://github.com/dcos/dcos-launch',
+        '09c1d53652d8e91aad5f1c246ef24646de2eb4c1',
     )
 
-    test_utils = 'git+https://github.com/dcos/dcos-test-utils@{sha}'.format(
-        sha=vendored_test_utils_sha,
+    test_utils = (
+        'dcos_test_utils',
+        'git+https://github.com/dcos/dcos-test-utils',
+        '00f1a62ef673ebc34e29d9db488dd06b0c1ae4ec',
     )
 
     # We have a fix at https://github.com/click-contrib/sphinx-click/pull/27
     # that we require.
     sphinx_click = (
-        'git+https://github.com/adamtheturtle/sphinx-click@'
-        'fix-envvar-duplicates'
+        'sphinx_click',
+        'git+https://github.com/adamtheturtle/sphinx-click',
+        'fix-envvar-duplicates',
     )
-
-    package_name_to_uri = {
-        'dcos_launch': launch,
-        'dcos_test_utils': test_utils,
-        'sphinx_click': sphinx_click,
-    }
 
     target_directory = Path('src/dcos_e2e/_vendor')
     try:
@@ -58,7 +54,11 @@ def main() -> None:
     init_file = Path(target_directory) / '__init__.py'
     Path(init_file).touch()
 
-    for _, requirement in package_name_to_uri.items():
+    package_names = set()
+    for requirement in [launch, test_utils, sphinx_click]:
+        package_name, uri, reference = requirement
+        package_names.add(package_name)
+
         subprocess.check_call(
             [
                 'pip',
@@ -66,7 +66,7 @@ def main() -> None:
                 '--no-dependencies',
                 '--target',
                 str(target_directory),
-                requirement,
+                uri + '@' + reference,
             ],
         )
 
@@ -75,7 +75,7 @@ def main() -> None:
     # from the vendored ``dcos_launch``.
     vendorize._rewrite_imports(  # pylint: disable=protected-access
         target_directory=target_directory,
-        top_level_names=list(package_name_to_uri.keys()),
+        top_level_names=list(package_names),
     )
 
 
