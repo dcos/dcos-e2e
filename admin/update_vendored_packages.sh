dd9ab0b84ddf8db49bf9c8ea3470c666bad6dee2
Adam Dangoor <adamdangoor@gmail.com>
2018-07-12 00:59:56 +0100

    More forgiving script

--- admin/update_vendored_packages.sh
+++ admin/update_vendored_packages.sh
@@ -6,7 +6,7 @@ set -ex
 
 # ``python-vendorize`` has problems with pip 10.0.0
 OLD_PIP=$(python -c 'import pkg_resources; print(pkg_resources.get_distribution("pip").parsed_version.public)')
-git rm -rf src/dcos_e2e/_vendor/
+git rm -rf src/dcos_e2e/_vendor/ || true
 rm -rf src/dcos_e2e/_vendor || true
 pip install pip==9.0.1
 python admin/update_vendored_packages.py
