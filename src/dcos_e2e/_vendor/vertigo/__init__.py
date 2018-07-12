fd9dff3ed336f7fa9be81a0066ce36cc7c3566ce
Adam Dangoor <adamdangoor@gmail.com>
2018-07-12 01:06:24 +0100

    Update vendored packages

---
+++ src/dcos_e2e/_vendor/vertigo/__init__.py
@@ -0,0 +1,1 @@
+from ..vertigo import VM
---
+++ src/dcos_e2e/_vendor/vertigo/setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='vertigo',
+    zip_safe=False,
+    packages=['vertigo'],
+    package_dir={'vertigo': ''},
+    include_package_data=True,
+)
