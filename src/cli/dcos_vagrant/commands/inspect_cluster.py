69e796f608425f2eb0cf735d4d28f54322478da1
Adam Dangoor <adamdangoor@gmail.com>
2018-07-16 01:38:33 +0200

    Working basic inspect command

--- src/cli/dcos_vagrant/commands/inspect_cluster.py
+++ src/cli/dcos_vagrant/commands/inspect_cluster.py
@@ -21,28 +21,13 @@ def inspect_cluster(cluster_id: str) -> None:
     master = next(iter(cluster_vms.cluster.masters))
     web_ui = 'http://' + str(master.private_ip_address)
     vagrant_client = cluster_vms.vagrant_client
-    import pdb; pdb.set_trace()
-    ssh_key = cluster_containers.workspace_dir / 'ssh' / 'id_rsa'
-
-    keys = {
-        'masters': cluster_containers.masters,
-        'agents': cluster_containers.agents,
-        'public_agents': cluster_containers.public_agents,
-    }
-
-    nodes = {
-        key: [
-            ContainerInspectView(container).to_dict()
-            for container in containers
-        ]
-        for key, containers in keys.items()
-    }
 
     data = {
         'Cluster ID': cluster_id,
         'Web UI': web_ui,
-        'Nodes': nodes,
-        'SSH key': str(ssh_key),
+        'Vagrant root': vagrant_client.root,
+        'SSH key': vagrant_client.keyfile(),
+        'SSH user': vagrant_client.user(),
     }  # type: Dict[Any, Any]
     click.echo(
         json.dumps(data, indent=4, separators=(',', ': '), sort_keys=True),
