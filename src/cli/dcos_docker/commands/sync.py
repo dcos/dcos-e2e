8bde7dad56a87fba2898d5df4dab1a2f50f6f023
Adam Dangoor <adamdangoor@gmail.com>
2018-07-15 18:52:24 +0100

    Move sync code helper to new file

--- src/cli/dcos_docker/commands/sync.py
+++ src/cli/dcos_docker/commands/sync.py
@@ -1,142 +1,16 @@
 """
-Tools for syncing code to a node.
+Tools for syncing code to a cluster.
 """
 
-import io
-import tarfile
-import tempfile
 from pathlib import Path
-from typing import Callable, Optional
 
 import click
 
-from dcos_e2e.cluster import Cluster
+from cli.common.sync import sync_code_to_masters
 from dcos_e2e.node import Transport
 
 from ._common import ClusterContainers
 from ._options import existing_cluster_id_option, node_transport_option
-
-
-def _tar_with_filter(
-    path: Path,
-    tar_filter: Callable[[tarfile.TarInfo], Optional[tarfile.TarInfo]],
-) -> io.BytesIO:
-    """
-    Return a tar of a files in a given directory, which are not filtered out
-    by the ``filter``.
-    """
-    tarstream = io.BytesIO()
-    with tarfile.TarFile(fileobj=tarstream, mode='w') as tar:
-        tar.add(name=str(path), arcname='/', filter=tar_filter)
-    tarstream.seek(0)
-
-    return tarstream
-
-
-def _cache_filter(tar_info: tarfile.TarInfo) -> Optional[tarfile.TarInfo]:
-    """
-    Filter for ``tarfile.TarFile.add`` which removes Python and pytest cache
-    files.
-    """
-    if '__pycache__' in tar_info.name:
-        return None
-    if tar_info.name.endswith('.pyc'):
-        return None
-    return tar_info
-
-
-def _sync_code_to_masters(cluster: Cluster, dcos_checkout_dir: Path) -> None:
-    """
-    Sync files from a DC/OS checkout to master nodes.
-
-    This syncs integration test files and bootstrap files.
-
-    This is not covered by automated tests, and it is non-trivial.
-
-    In the following instructions, running a test might look like:
-
-    `dcos-docker run pytest <test_filename>`
-
-    The manual test cases we want to work are:
-    * Sync a DC/OS Enterprise checkout and run a test - it should work.
-    * Delete a test file, sync, try to run this test file - it should fail
-      with "file not found".
-    * Add a test file, sync, try to run this test file - it should work.
-    * Add `assert False`, sync, to a test file and run this test file - it
-      should fail.
-    * Test bootstrap sync with no changes (a partial test that nothing
-      breaks):
-      - Sync
-      - `dcos-docker run systemctl restart dcos-mesos-master`
-      - `dcos-docker run journalctl -f -u dcos-mesos-master`
-      - We expect to see no assertion error.
-    * Test bootstrap sync with some changes
-      - Add `assert False` to
-        `packages/bootstrap/extra/dcos_internal_utils/bootstrap.py`
-      - `dcos-docker run systemctl restart dcos-mesos-master`
-      - `dcos-docker run journalctl -f -u dcos-mesos-master`
-      - We expect to see the assertion error.
-    """
-    local_packages = dcos_checkout_dir / 'packages'
-    local_test_dir = local_packages / 'dcos-integration-test' / 'extra'
-    if not Path(local_test_dir).exists():
-        message = (
-            'DCOS_CHECKOUT_DIR must be set to the checkout of a DC/OS '
-            'repository.\n'
-            '"{local_test_dir}" does not exist.'
-        ).format(local_test_dir=local_test_dir)
-        raise click.BadArgumentUsage(message=message)
-    node_active_dir = Path('/opt/mesosphere/active')
-    node_test_dir = node_active_dir / 'dcos-integration-test'
-    node_lib_dir = node_active_dir / 'bootstrap' / 'lib'
-    # Different versions of DC/OS have different versions of Python.
-    master = next(iter(cluster.masters))
-    ls_result = master.run(args=['ls', str(node_lib_dir)])
-    python_version = ls_result.stdout.decode().strip()
-    node_python_dir = node_lib_dir / python_version
-    node_bootstrap_dir = (
-        node_python_dir / 'site-packages' / 'dcos_internal_utils'
-    )
-
-    local_bootstrap_dir = (
-        local_packages / 'bootstrap' / 'extra' / 'dcos_internal_utils'
-    )
-
-    test_tarstream = _tar_with_filter(
-        path=local_test_dir,
-        tar_filter=_cache_filter,
-    )
-    bootstrap_tarstream = _tar_with_filter(
-        path=local_bootstrap_dir,
-        tar_filter=_cache_filter,
-    )
-
-    node_test_py_pattern = node_test_dir / '*.py'
-    tar_path = '/tmp/dcos_e2e_tmp.tar'
-    for master in cluster.masters:
-        master.run(
-            args=['rm', '-rf', str(node_test_py_pattern)],
-            # We use a wildcard character, `*`, so we need shell expansion.
-            shell=True,
-        )
-
-        for tarstream, node_destination in (
-            (test_tarstream, node_test_dir),
-            (bootstrap_tarstream, node_bootstrap_dir),
-        ):
-
-            with tempfile.NamedTemporaryFile() as tmp_file:
-                tmp_file.write(tarstream.getvalue())
-                tmp_file.flush()
-
-                master.send_file(
-                    local_path=Path(tmp_file.name),
-                    remote_path=Path(tar_path),
-                )
-
-            tar_args = ['tar', '-C', str(node_destination), '-xvf', tar_path]
-            master.run(args=tar_args)
-            master.run(args=['rm', tar_path])
 
 
 @click.command('sync')
@@ -172,7 +46,7 @@ def sync_code(
         transport=transport,
     )
     cluster = cluster_containers.cluster
-    _sync_code_to_masters(
+    sync_code_to_masters(
         cluster=cluster,
         dcos_checkout_dir=Path(dcos_checkout_dir),
     )
