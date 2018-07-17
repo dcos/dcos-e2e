22f20b8caa5dbd7f8c152ff0c60520f63e38d29d
Tim Weidner <tweidner@mesosphere.com>
2018-07-17 13:41:00 +0200

    tests passing

--- tests/test_dcos_e2e/scratch/test_ip_detect.py
+++ tests/test_dcos_e2e/scratch/test_ip_detect.py
@@ -13,46 +13,6 @@ class TestCustomIPDetect:
     Users can specify a custom ``ip-detect``.
     """
 
-    def test_install_node_from_path(
-        self,
-        cluster_backend: ClusterBackend,
-        oss_artifact: Path,
-        tmpdir: local,
-    ) -> None:
-        """
-        Install DC/OS with a custom ``ip-detect`` script node by node.
-        """
-        with Cluster(
-            cluster_backend=cluster_backend,
-            masters=1,
-            agents=0,
-            public_agents=0,
-        ) as cluster:
-
-            (master, ) = cluster.masters
-            ip_detect_file = tmpdir.join('ip-detect')
-            ip_detect_contents = dedent(
-                """\
-                #!/bin/bash
-                echo {ip_address}
-                """,
-            ).format(ip_address=master.private_ip_address)
-            ip_detect_file.write(ip_detect_contents)
-
-            master.install_dcos_from_path(
-                build_artifact=oss_artifact,
-                dcos_config=cluster.base_config,
-                role=Role.MASTER,
-                files_to_copy_to_genconf_dir=[
-                    (Path(str(ip_detect_file)), Path('/genconf/ip-detect')),
-                ],
-            )
-            cluster.wait_for_dcos_oss()
-            cat_result = master.run(
-                args=['cat', '/opt/mesosphere/bin/detect_ip'],
-            )
-            assert cat_result.stdout.decode() == ip_detect_contents
-
     def test_install_cluster_from_path(
         self,
         cluster_backend: ClusterBackend,
