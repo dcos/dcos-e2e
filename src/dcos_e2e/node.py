1dab46e5095aa38283f59201105880f3cf36b715
Tim Weidner <tweidner@mesosphere.com>
2018-07-20 13:27:28 +0200

    Update test names

--- src/dcos_e2e/node.py
+++ src/dcos_e2e/node.py
@@ -9,7 +9,7 @@ from enum import Enum
 from ipaddress import IPv4Address
 from pathlib import Path
 from tempfile import gettempdir
-from typing import Any, Dict, Iterable, List, Optional, Tuple
+from typing import Any, Dict, List, Optional
 
 import yaml
 
