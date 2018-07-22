8cb7acf534d0648ad1d3a1afcec92d36840fa865
Adam Dangoor <adamdangoor@gmail.com>
2018-07-22 13:55:20 +0100

    Start of new command

--- src/cli/common/commands.py
+++ src/cli/common/commands.py
@@ -28,3 +28,25 @@ def list_clusters_command_factory(
 
     list_clusters_func = list_clusters  # type: Callable[[None], None]
     return list_clusters_func
+
+
+@click.command('download-artifact')
+def download_artifact() -> None:
+    """
+    List all clusters.
+    """
+    label = 'Downloading to ' + str(path)
+    stream = requests.get(url, stream=True)
+    content_length = int(stream.headers['Content-Length'])
+    chunk_size = 100 * 1024
+    with click.open_file(str(path), 'wb') as file_descriptor:
+        content_iter = stream.iter_content(chunk_size=chunk_size)
+        with click.progressbar(  # type: ignore
+            content_iter,
+            length=content_length / chunk_size,
+            label=label,
+        ) as progress_bar:
+            for chunk in progress_bar:
+                if chunk:
+                    file_descriptor.write(chunk)  # type: ignore
+                    file_descriptor.flush()  # type: ignore
