202086fba48d332895da8528d1e5717de829ceb2
Adam Dangoor <adamdangoor@gmail.com>
2018-07-22 21:28:35 +0100

    Add test for create

--- tests/test_cli/test_dcos_aws/test_cli.py
+++ tests/test_cli/test_dcos_aws/test_cli.py
@@ -72,3 +72,92 @@ class TestDcosAWS:
         )
         # yapf: enable
         assert result.output == expected_help
+
+
+class TestCreate:
+    """
+    Tests for the `create` subcommand.
+    """
+
+    def test_help(self) -> None:
+        """
+        Help text is shown with `dcos-aws create --help`.
+        """
+        runner = CliRunner()
+        result = runner.invoke(
+            dcos_aws,
+            ['create', '--help'],
+            catch_exceptions=False,
+        )
+        assert result.exit_code == 0
+        # yapf breaks multi-line noqa, see
+        # https://github.com/google/yapf/issues/524.
+        # yapf: disable
+        expected_help = dedent(
+            """\
+            Usage: dcos-aws create [OPTIONS]
+
+              Create a DC/OS cluster.
+
+                  DC/OS Enterprise
+
+                              DC/OS Enterprise clusters require different configuration variables to DC/OS OSS.
+                              For example, enterprise clusters require the following configuration parameters:
+
+                      ``superuser_username``, ``superuser_password_hash``,
+                      ``fault_domain_enabled``, ``license_key_contents``
+
+                              These can all be set in ``--extra-config``.
+                              However, some defaults are provided for all but the license key.
+
+                              The default superuser username is ``admin``.
+                              The default superuser password is ``admin``.
+                              The default ``fault_domain_enabled`` is ``false``.
+
+                              ``license_key_contents`` must be set for DC/OS Enterprise 1.11 and above.
+                              This is set to one of the following, in order:
+
+                              * The ``license_key_contents`` set in ``--extra-config``.
+                              * The contents of the path given with ``--license-key``.
+                              * The contents of the path set in the ``DCOS_LICENSE_KEY_PATH`` environment variable.
+
+                              If none of these are set, ``license_key_contents`` is not given.
+
+            Options:
+              --variant [oss|enterprise]      Choose the DC/OS variant. If the variant does
+                                              not match the variant of the given artifact
+                                              URL, an error will occur.
+              --masters INTEGER               The number of master nodes.  [default: 1]
+              --agents INTEGER                The number of agent nodes.  [default: 1]
+              --extra-config PATH             The path to a file including DC/OS
+                                              configuration YAML. The contents of this file
+                                              will be added to add to a default
+                                              configuration.
+              --public-agents INTEGER         The number of public agent nodes.  [default:
+                                              1]
+              --workspace-dir PATH            Creating a cluster can use approximately 2 GB
+                                              of temporary storage. Set this option to use a
+                                              custom "workspace" for this temporary storage.
+                                              See https://docs.python.org/3/library/tempfile
+                                              .html#tempfile.gettempdir for details on the
+                                              temporary directory location if this option is
+                                              not set.
+              --license-key PATH              This is ignored if using open source DC/OS. If
+                                              using DC/OS Enterprise, this defaults to the
+                                              value of the `DCOS_LICENSE_KEY_PATH`
+                                              environment variable.
+              --security-mode [disabled|permissive|strict]
+                                              The security mode to use for a DC/OS
+                                              Enterprise cluster. This overrides any
+                                              security mode set in ``--extra-config``.
+              --copy-to-master TEXT           Files to copy to master nodes before
+                                              installing DC/OS. This option can be given
+                                              multiple times. Each option should be in the
+                                              format /absolute/local/path:/remote/path.
+              -v, --verbose                   Use verbose output. Use this option multiple
+                                              times for more verbose output.
+              --help                          Show this message and exit.
+            """,# noqa: E501,E261
+        )
+        # yapf: enable
+        assert result.output == expected_help
