74ccbf3afa5aa1170f218944b6d70fd16e242b39
Adam Dangoor <adamdangoor@gmail.com>
2018-07-23 13:30:29 +0100

    Progress towards listing clusters

--- src/cli/dcos_aws/commands/_common.py
+++ src/cli/dcos_aws/commands/_common.py
@@ -2,17 +2,19 @@
 Common code for dcos-docker CLI modules.
 """
 
+from typing import Set
+
+import boto3
+
 CLUSTER_ID_TAG_KEY = 'dcos_e2e.cluster_id'
 
 def existing_cluster_ids() -> Set[str]:
     """
     Return the IDs of existing clusters.
     """
-    # TODO Maybe hardcode region for now?
-    # Take option later?
-    client = docker_client()
-    filters = {'label': CLUSTER_ID_LABEL_KEY}
-    containers = client.containers.list(filters=filters)
-    return set(
-        container.labels[CLUSTER_ID_LABEL_KEY] for container in containers
-    )
+    ec2 = boto3.client('ec2')
+
+    # Retrieves all regions/endpoints that work with EC2
+    response = ec2.describe_regions()
+    print('Regions:', response['Regions'])
+    # TODO fill this out
