7fc2e9e7a3800c89ab8e23b6f343a6f3af63d458
Adam Dangoor <adamdangoor@gmail.com>
2018-07-23 17:53:30 +0100

    Add SSH check to AWS doctor

--- src/cli/dcos_aws/commands/doctor.py
+++ src/cli/dcos_aws/commands/doctor.py
@@ -2,7 +2,11 @@
 Checks for showing up common sources of errors with the AWS backend.
 """
 
+import sys
+
 import click
+
+from cli.common.doctor import CheckLevels, check_ssh
 
 
 @click.command('doctor')
@@ -10,4 +14,8 @@ def doctor() -> None:
     """
     Diagnose common issues which stop DC/OS E2E from working correctly.
     """
-    # There are currently no doctor checks.
+    check_functions = [check_ssh]
+    for function in check_functions:
+        level = function()
+        if level == CheckLevels.ERROR:
+            sys.exit(1)
