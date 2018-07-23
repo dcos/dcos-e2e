164c9d3a54ce2ad8d01fa5be63184a791095c364
Adam Dangoor <adamdangoor@gmail.com>
2018-07-23 14:11:57 +0000

    Update for release 2018.07.23.1

--- dcose2e.rb
+++ dcose2e.rb
@@ -1,7 +1,7 @@
 class Dcose2e < Formula
   include Language::Python::Virtualenv
 
-  url "https://github.com/dcos/dcos-e2e/archive/2018.07.23.0.tar.gz"
+  url "https://github.com/dcos/dcos-e2e/archive/2018.07.23.1.tar.gz"
   head "https://github.com/dcos/dcos-e2e.git"
   homepage "http://dcos-e2e.readthedocs.io/en/latest/cli.html"
   depends_on "python3"
