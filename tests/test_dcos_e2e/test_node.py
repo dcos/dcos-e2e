5f9c7f183559a6dcf028701ff87d9da94877afcd
Adam Dangoor <adamdangoor@gmail.com>
2018-07-25 16:32:25 +0100

    Move TestCopyFiles [skip ci]

--- tests/test_dcos_e2e/test_node.py
+++ tests/test_dcos_e2e/test_node.py
@@ -10,7 +10,6 @@ import uuid
 from ipaddress import IPv4Address
 from pathlib import Path
 from subprocess import CalledProcessError, TimeoutExpired
-from textwrap import dedent
 from typing import Iterator
 
 import pytest
@@ -20,7 +19,7 @@ from _pytest.logging import LogCaptureFixture
 # are disabled.
 from py.path import local  # pylint: disable=no-name-in-module, import-error
 
-from dcos_e2e.backends import ClusterBackend, Docker
+from dcos_e2e.backends import Docker
 from dcos_e2e.cluster import Cluster
 from dcos_e2e.node import Node, Transport
 
@@ -47,56 +46,6 @@ def dcos_node(request: SubRequest) -> Iterator[Node]:
     ) as cluster:
         (master, ) = cluster.masters
         yield master
-
-
-class TestCopyFiles:
-    """
-    Test copying files to the ``genconf`` directory before installing
-    DC/OS on a node.
-    """
-
-    def test_install_node_from_path(
-        self,
-        cluster_backend: ClusterBackend,
-        oss_artifact: Path,
-        tmpdir: local,
-    ) -> None:
-        """
-        It is possible to copy files to the ``genconf`` directory.
-        """
-        with Cluster(
-            cluster_backend=cluster_backend,
-            masters=1,
-            agents=0,
-            public_agents=0,
-        ) as cluster:
-
-            (master, ) = cluster.masters
-            ip_detect_file = tmpdir.join('ip-detect')
-            ip_detect_contents = dedent(
-                """\
-                #!/bin/bash
-                echo {ip_address}
-                """,
-            ).format(ip_address=master.private_ip_address)
-            ip_detect_file.write(ip_detect_contents)
-
-            master.install_dcos_from_path(
-                build_artifact=oss_artifact,
-                dcos_config=cluster.base_config,
-                ip_detect_path=cluster_backend.ip_detect_path,
-                # Test that this overwrites the ``ip-detect`` script given
-                # by ``ip_detect_path``.
-                files_to_copy_to_genconf_dir=[
-                    (Path(str(ip_detect_file)), Path('/genconf/ip-detect')),
-                ],
-                role=Role.MASTER,
-            )
-            cluster.wait_for_dcos_oss()
-            cat_result = master.run(
-                args=['cat', '/opt/mesosphere/bin/detect_ip'],
-            )
-            assert cat_result.stdout.decode() == ip_detect_contents
 
 
 class TestEquality:
