3940f74d7648e8a2c38bde825413c48bfdacf97f
Tim Weidner <tweidner@mesosphere.com>
2018-07-25 13:00:10 +0200

    Add test for coverage

--- tests/test_dcos_e2e/backends/aws/test_aws.py
+++ tests/test_dcos_e2e/backends/aws/test_aws.py
@@ -4,6 +4,7 @@ Tests for the AWS backend.
 
 import uuid
 from pathlib import Path
+from textwrap import dedent
 
 import boto3
 import pytest
@@ -283,3 +284,37 @@ class TestDCOSInstallation:
                 ip_detect_path=cluster_backend.ip_detect_path,
             )
             cluster.wait_for_dcos_oss()
+
+    def test_install_dcos_from_url_custom_ip_detect(
+        self,
+        oss_artifact_url: str,
+        tmpdir: local,
+    ) -> None:
+        """
+        It is possible to install DC/OS on an AWS cluster with a custom
+        ``ip-detect`` script.
+        """
+        with Cluster(
+            cluster_backend=AWS(),
+            agents=0,
+            public_agents=0,
+        ) as cluster:
+            (master, ) = cluster.masters
+            ip_detect_file = tmpdir.join('ip-detect')
+            ip_detect_contents = dedent(
+                """\
+                #!/bin/bash
+                echo {ip_address}
+                """,
+            ).format(ip_address=master.private_ip_address)
+            ip_detect_file.write(ip_detect_contents)
+            cluster.install_dcos_from_url(
+                build_artifact=oss_artifact_url,
+                dcos_config=cluster.base_config,
+                ip_detect_path=Path(str(ip_detect_file)),
+            )
+            cluster.wait_for_dcos_oss()
+            cat_result = master.run(
+                args=['cat', '/opt/mesosphere/bin/detect_ip'],
+            )
+            assert cat_result.stdout.decode() == ip_detect_contents
