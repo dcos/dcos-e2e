3cc812afe0901649f498f0839662076982842d73
Adam Dangoor <adamdangoor@gmail.com>
2018-07-26 17:20:39 +0100

    Use better server config

--- src/dcos_e2e/cluster.py
+++ src/dcos_e2e/cluster.py
@@ -211,7 +211,7 @@ class Cluster(ContextDecorator):
             '&&',
             'zkCli.sh',
             '-server',
-            'zk-1.zk:2181,zk-2.zk:2181,zk-3.zk:2181,zk-4.zk:2181,zk-5.zk:2181',
+            '"zk-1.zk:2181,zk-2.zk:2181,zk-3.zk:2181,zk-4.zk:2181,zk-5.zk:2181"',
             'delete',
             path,
         ]
