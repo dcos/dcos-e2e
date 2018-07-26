64daafba5a3f7d2d646406d0a7b341f40aca61b4
Adam Dangoor <adamdangoor@gmail.com>
2018-07-27 00:02:50 +0100

    Fix lint

--- src/cli/dcos_aws/commands/wait.py
+++ src/cli/dcos_aws/commands/wait.py
@@ -12,10 +12,15 @@ from cli.common.options import (
     superuser_username_option,
     verbosity_option,
 )
-from cli.common.utils import check_cluster_id_exists, set_logging
+from cli.common.utils import (
+    check_cluster_id_exists,
+    set_logging,
+    show_wait_help,
+)
 
 from ._common import ClusterInstances, existing_cluster_ids
 from ._options import aws_region_option
+from .doctor import doctor
 
 
 @click.command('wait')
@@ -24,7 +29,9 @@ from ._options import aws_region_option
 @superuser_password_option
 @verbosity_option
 @aws_region_option
+@click.pass_context
 def wait(
+    ctx: click.core.Context,
     cluster_id: str,
     superuser_username: str,
     superuser_password: str,
@@ -40,17 +47,19 @@ def wait(
     )
     set_logging(verbosity_level=verbose)
     urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-    message = (
-        'A cluster may take some time to be ready.\n'
-        'The amount of time it takes to start a cluster depends on a variety '
-        'of factors.\n'
-        'If you are concerned that this is hanging, try "dcos-aws doctor" '
-        'to diagnose common issues.'
-    )
-    click.echo(message)
     cluster_instances = ClusterInstances(
         cluster_id=cluster_id,
         aws_region=aws_region,
+    )
+    parent = ctx.parent
+    assert parent is not None
+    doctor_command_name = '{info_name} {doctor_name}'.format(
+        info_name=parent.info_name,
+        doctor_name=doctor.name,
+    )
+    show_wait_help(
+        is_enterprise=cluster_instances.is_enterprise,
+        doctor_command_name=doctor_command_name,
     )
     with click_spinner.spinner():
         if cluster_instances.is_enterprise:
