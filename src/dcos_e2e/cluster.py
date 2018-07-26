03dc152cf6625b9dbae58ff1c024e52a706f4ec9
Adam Dangoor <adamdangoor@gmail.com>
2018-07-26 16:45:38 +0100

    Try explicit servers

--- src/dcos_e2e/cluster.py
+++ src/dcos_e2e/cluster.py
@@ -210,6 +210,8 @@ class Cluster(ContextDecorator):
             '/opt/mesosphere/environment.export',
             '&&',
             'zkCli.sh',
+            '--servers',
+            'zk-1.zk:2181,zk-2.zk:2181,zk-3.zk:2181,zk-4.zk:2181,zk-5.zk:2181',
             'delete',
             path,
         ]
