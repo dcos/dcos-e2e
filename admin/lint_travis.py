a18ab76b93862d7025b040ccd0d946316dc4ac07
Adam Dangoor <adamdangoor@gmail.com>
2018-07-29 08:51:09 +0100

    Remove done line

--- admin/lint_travis.py
+++ admin/lint_travis.py
@@ -53,32 +53,30 @@ def test_ci_patterns_match() -> None:
 
 
 if __name__ == '__main__':
-    pass
-    # CI_PATTERNS = travis_ci_patterns()
-    # if CI_PATTERNS != PATTERNS.keys():
-    #
-    # COLLECT_ONLY_ERROR_RESULTS = set()
-    # for CI_PATTERN in CI_PATTERNS:
-    #     OLD_OUT = sys.stdout
-    #     OLD_ERR = sys.stderr
-    #     sys.stdout = open(os.devnull, 'w')
-    #     sys.stderr = open(os.devnull, 'w')
-    #     COLLECT_ONLY_RESULT = pytest.main(['--collect-only', CI_PATTERN])
-    #     sys.stdout = OLD_OUT
-    #     sys.stderr = OLD_ERR
-    #
-    #     if COLLECT_ONLY_RESULT != 0:
-    #         COLLECT_ONLY_ERROR_RESULTS.add(CI_PATTERN)
-    #
-    # for ERROR_PATTERN in COLLECT_ONLY_ERROR_RESULTS:
-    #     sys.stderr.write(
-    #         'Error finding tests with pattern "{pattern}".\n'.format(
-    #             pattern=ERROR_PATTERN,
-    #         ),
-    #     )
-    #
-    # if CI_PATTERNS != PATTERNS.keys() or COLLECT_ONLY_ERROR_RESULTS:
-    #     sys.exit(1)
-    #
-    # # TODO are there tests duplicated across patterns
-    # # TODO are there tests missing
+    CI_PATTERNS = _travis_ci_patterns()
+
+    COLLECT_ONLY_ERROR_RESULTS = set()
+    for CI_PATTERN in CI_PATTERNS:
+        OLD_OUT = sys.stdout
+        OLD_ERR = sys.stderr
+        sys.stdout = open(os.devnull, 'w')
+        sys.stderr = open(os.devnull, 'w')
+        COLLECT_ONLY_RESULT = pytest.main(['--collect-only', CI_PATTERN])
+        sys.stdout = OLD_OUT
+        sys.stderr = OLD_ERR
+
+        if COLLECT_ONLY_RESULT != 0:
+            COLLECT_ONLY_ERROR_RESULTS.add(CI_PATTERN)
+
+    for ERROR_PATTERN in COLLECT_ONLY_ERROR_RESULTS:
+        sys.stderr.write(
+            'Error finding tests with pattern "{pattern}".\n'.format(
+                pattern=ERROR_PATTERN,
+            ),
+        )
+
+    if CI_PATTERNS != PATTERNS.keys() or COLLECT_ONLY_ERROR_RESULTS:
+        sys.exit(1)
+
+    # TODO are there tests duplicated across patterns
+    # TODO are there tests missing
