2a46522b1d9f5a2d730cc9cb9632f1bcb97b2079
Tim Weidner <tweidner@mesosphere.com>
2018-07-30 14:52:15 +0200

    Split tests

--- admin/run_script.py
+++ admin/run_script.py
@@ -81,6 +81,7 @@ PATTERNS = {
     'tests/test_dcos_e2e/backends/aws/test_distributions.py::TestRHEL7::test_oss':  # noqa: E501
     (),
     'tests/test_dcos_e2e/backends/aws/test_distributions.py::TestRHEL7::test_enterprise':  # noqa: E501
+    (),
     'tests/test_dcos_e2e/backends/aws/test_aws.py::TestTags':  # noqa: E501
     (),
     'tests/test_dcos_e2e/backends/docker/test_distributions.py::TestCentos7':
