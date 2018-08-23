400064d8b862449d4337114d9fe3e5bc102093ca
Adam Dangoor <adamdangoor@gmail.com>
2018-08-23 12:00:26 +0100

    Fix test docstring

--- admin/custom_linters.py
+++ admin/custom_linters.py
@@ -52,7 +52,7 @@ def _tests_from_pattern(ci_pattern: str) -> Set[str]:
 def test_ci_patterns_match() -> None:
     """
     The patterns in ``.travis.yml`` must match the patterns in
-    ``admin/run_script.py``.
+    ``admin/download_artfacts.py``.
     """
     ci_patterns = _travis_ci_patterns()
     assert ci_patterns - PATTERNS.keys() == set()
