25cedbc1e8a2a86fafd7c1ce377fa6bb308a3f50
Adam Dangoor <adamdangoor@gmail.com>
2018-09-06 17:26:25 +0100

    Start of test brew

---
+++ admin/test_brew.py
@@ -0,0 +1,14 @@
+"""
+"""
+
+def test_brew():
+    """
+    XXX
+    """
+    # TODO make archive
+    # TODO admin/homebrew
+    # TODO write file
+    # TODO start container
+    # docker run -it linuxbrew/linuxbrew
+    # TODO send file to container
+    # TODO install from Linuxbrew
