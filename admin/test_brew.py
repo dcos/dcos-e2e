daa1e7c8b9085579761762ee06a1ed68654a00a7
Adam Dangoor <adamdangoor@gmail.com>
2018-09-06 18:15:06 +0100

    Progress towards test for brew

--- admin/test_brew.py
+++ admin/test_brew.py
@@ -6,6 +6,7 @@ def test_brew():
     XXX
     """
     # TODO make archive - test can you do archive from file:///
+    # git archive --format=tar.gz -o /tmp/my-repo.tar.gz --prefix=my-repo/ master
     # If so, this probably needs to be in the container
     # TODO admin/homebrew
     # TODO write file
