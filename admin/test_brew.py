d987b07a8377f1dfbaae3c0a888a4ea2c6ae42a4
Adam Dangoor <adamdangoor@gmail.com>
2018-09-06 18:11:35 +0100

    Progress towards test for brew

--- admin/test_brew.py
+++ admin/test_brew.py
@@ -5,7 +5,8 @@ def test_brew():
     """
     XXX
     """
-    # TODO make archive
+    # TODO make archive - test can you do archive from file:///
+    # If so, this probably needs to be in the container
     # TODO admin/homebrew
     # TODO write file
     # TODO start container
