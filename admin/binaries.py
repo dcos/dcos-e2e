0683702628a8b337fd01f4d077eb89fd49951273
Adam Dangoor <adamdangoor@gmail.com>
2018-10-11 16:56:02 +0100

    Progress

--- admin/binaries.py
+++ admin/binaries.py
@@ -31,65 +31,20 @@ def make_linux_binaries(repo_root: Path) -> Set[Path]:
         type='bind',
     )
 
-    # MANIFEST.in describes files that must be available which are not
-    # necessarily Python files.
-    # These include e.g. Dockerfiles.
-    # We still need to include these in the binary.
-    datas = []
-    with open('MANIFEST.in') as manifest_file:
-        for line in manifest_file.readlines():
-            if line.startswith('recursive-include'):
-                _, manifest_path, _ = line.split()
-            else:
-                _, manifest_path = line.split()
-            if manifest_path.startswith('src/'):
-                if Path(manifest_path).is_file():
-                    parent = Path(manifest_path).parent
-                    manifest_path = str(parent)
-
-                path_without_src = manifest_path[len('src/'):]
-                datas.append((manifest_path, path_without_src))
-
-    dist_dir = repo_root / 'dist'
-    for path in list(repo_root.glob('dcos-*.spec')) + [dist_dir]:
-        container_path = Path(target_dir) / str(path.relative_to(repo_root))
-        container = client.containers.run(
-            image='python:3.6',
-            mounts=[code_mount],
-            command=['rm', '-rf', str(container_path)],
-            working_dir=target_dir,
-            remove=True,
-            detach=True,
-        )
-        for line in container.logs(stream=True):
-            line = line.strip()
-            LOGGER.info(line)
-
-    bin_dir = repo_root / 'bin'
-    binaries = list(bin_dir.iterdir())
-
     # We explicitly do not use ``-e / --editable``.
     # This is because ``versioneer`` replaces the dynamic ``_version.py`` file
     # with a static one only when creating a non-editable Python EGG.  This is
     # required for the PyInstaller binary to determine the version string
     # because the git tags used by the dynamic ``_version.py`` are not
     # included.
-    cmd_in_container = ['pip3', 'install', '.[packaging]']
-    for binary in binaries:
-        cmd_in_container += [
-            '&&',
-            'pyinstaller',
-            './bin/{binary}'.format(binary=binary.name),
-            '--onefile',
-        ]
-        for data in datas:
-            source, destination = data
-            data_str = '{source}:{destination}'.format(
-                source=source,
-                destination=destination,
-            )
-            add_data_command = ['--add-data', data_str]
-            cmd_in_container += add_data_command
+    cmd_in_container = [
+        'pip3',
+        'install',
+        '.[packaging]',
+        '&&',
+        'python',
+        'admin/create_pyinstaller_binaries.py',
+    ]
     cmd = 'bash -c "{cmd}"'.format(cmd=' '.join(cmd_in_container))
 
     container = client.containers.run(
@@ -104,4 +59,5 @@ def make_linux_binaries(repo_root: Path) -> Set[Path]:
         line = line.strip()
         LOGGER.info(line)
 
-    return set(repo_root / 'dist' / binary.name for binary in binaries)
+    dist_dir = repo_root / 'dist'
+    return set(dist_dir.iterdir())
--- admin/create_pyinstaller_binaries.py
+++ admin/create_pyinstaller_binaries.py
@@ -1,8 +1,59 @@
+"""
+Make PyInstaller binaries for the platform that this is being run on.
+
+For this to work as expected, we expect that NOT EDITABLE.
+"""
+
 import shutil
+import subprocess
+from pathlib import Path
 
-# Remove dist
-# Remove dcos-*.spec
 
-# Get the add data command
+def remove_existing_files(repo_root: Path) -> None:
+    dist_dir = repo_root / 'dist'
+    if dist_dir.exists():
+        for path in repo_root.glob('dcos-*.spec'):
+            path.unlink()
 
-# Run the whole command with MANIFEST
+        shutil.rmtree(path=dist_dir)
+
+
+def create_binary(script: Path) -> None:
+    # MANIFEST.in describes files that must be available which are not
+    # necessarily Python files.
+    # These include e.g. Dockerfiles.
+    # We still need to include these in the binary.
+    datas = []
+    with open('MANIFEST.in') as manifest_file:
+        for line in manifest_file.readlines():
+            if line.startswith('recursive-include'):
+                _, manifest_path, _ = line.split()
+            else:
+                _, manifest_path = line.split()
+            if manifest_path.startswith('src/'):
+                if Path(manifest_path).is_file():
+                    parent = Path(manifest_path).parent
+                    manifest_path = str(parent)
+
+                path_without_src = manifest_path[len('src/'):]
+                datas.append((manifest_path, path_without_src))
+
+    pyinstaller_command = ['pyinstaller', script.resolve(), '--onefile']
+    for data in datas:
+        source, destination = data
+        data_str = '{source}:{destination}'.format(
+            source=source,
+            destination=destination,
+        )
+        add_data_command = ['--add-data', data_str]
+        pyinstaller_command += add_data_command
+
+    subprocess.check_output(args=pyinstaller_command)
+
+
+if __name__ == '__main__':
+    repo_root = Path(__file__).parent.parent
+    remove_existing_files(repo_root=repo_root)
+    script_dir = repo_root / 'bin'
+    for script in script_dir.iterdir():
+        create_binary(script=script)
