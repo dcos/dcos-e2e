332cb874c028164b14df40c884ca60250fd563eb
Adam Dangoor <adamdangoor@gmail.com>
2018-10-15 13:12:34 +0100

    Progress

--- admin/update_cli_tests.sh
+++ admin/update_cli_tests.sh
@@ -2,7 +2,7 @@
 
 set -ex
 
-git rm -r tests/test_cli/test_dcos_docker/test_cli/help_output
+git rm -r tests/test_cli/test_dcos_docker/help_outputs || true
 export FIX_CLI_TESTS=1
-pytest tests/test_cli/test_dcos_docker/test_cli::TestHelp::test_help
-git add tests/test_cli/test_dcos_docker/test_cli/help_output/*
+pytest tests/test_cli/test_dcos_docker/test_cli.py::TestHelp::test_help
+git add tests/test_cli/test_dcos_docker/help_outputs/*.txt
