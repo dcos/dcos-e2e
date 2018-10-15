4d50b7f9b20b17e48d20a366ddf60a18711c974a
Adam Dangoor <adamdangoor@gmail.com>
2018-10-15 13:28:48 +0100

    Progress

--- admin/update_cli_tests.sh
+++ admin/update_cli_tests.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 
+# There are CLI tests which check that --help output for various commands is
+# as expected.
+#
+# Expected output is stored in files.
+
 set -ex
 
-mkdir tests/test_cli/test_dcos_docker/help_outputs || true
+mkdir -p tests/test_cli/test_dcos_docker/help_outputs
 git rm -f tests/test_cli/test_dcos_docker/help_outputs/*.txt || true
 export FIX_CLI_TESTS=1
 pytest tests/test_cli/test_dcos_docker/test_cli.py::TestHelp::test_help || true
