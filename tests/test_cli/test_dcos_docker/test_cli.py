8432ae571ab9eba1d8090c7388976d5f4de81012
Adam Dangoor <adamdangoor@gmail.com>
2018-10-15 13:20:26 +0100

    Progress

--- tests/test_cli/test_dcos_docker/test_cli.py
+++ tests/test_cli/test_dcos_docker/test_cli.py
@@ -47,7 +47,7 @@ class TestHelp:
     Test help texts.
     """
 
-    @pytest.mark.parametrize('command', list(dcos_docker.commands.keys()))
+    @pytest.mark.parametrize('command', [] + [[item] for item in dcos_docker.commands.keys()])
     def test_help(
         self,
         command: List[str],
