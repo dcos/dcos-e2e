3fe285cbcc177ea031c499fa0ef17817aee5d625
Adam Dangoor <adamdangoor@gmail.com>
2018-10-15 13:27:01 +0100

    Progress

--- tests/test_cli/test_dcos_docker/test_cli.py
+++ tests/test_cli/test_dcos_docker/test_cli.py
@@ -71,6 +71,7 @@ class TestHelp:
             assert result.output == expected_help
         except (AssertionError, FileNotFoundError):  # pragma: no cover
             if os.getenv('FIX_CLI_TESTS') == '1':
+                help_outputs_dir.mkdir(exist_ok=True)
                 expected_help_file.touch()
                 expected_help_file.write_text(result.output)
             else:
