93ed1fe6aad2b5f367015e711689680f71b388b7
Jan Schlicht <jan@mesosphere.io>
2018-11-23 13:16:19 +0100

    Test that created loopback devices have expected size.

--- tests/test_dcos_e2e/docker_utils/test_loopback.py
+++ tests/test_dcos_e2e/docker_utils/test_loopback.py
@@ -18,20 +18,34 @@ class TestDockerLoopbackVolume:
         client = docker.from_env(version='auto')
 
         with DockerLoopbackVolume(size=1) as device:
-            block_device_exists_cmd = ['lsblk', device.path]
             new_container = client.containers.create(
                 privileged=True,
                 detach=True,
                 image='centos:7',
             )
             new_container.start()
-            exit_code, output = new_container.exec_run(
-                cmd=block_device_exists_cmd,
-            )
-            new_container.stop()
-            new_container.remove()
 
-            assert exit_code == 0, device.path + ': ' + output.decode()
+            try:
+                block_device_exists_cmd = ['lsblk', device.path]
+                exit_code, output = new_container.exec_run(
+                    cmd=block_device_exists_cmd,
+                )
+                assert exit_code == 0, device.path + ': ' + output.decode()
+
+                block_device_has_right_size = [
+                    'stat',
+                    '-f',
+                    '"%z"',
+                    device.path,
+                ]
+                exit_code, output = new_container.exec_run(
+                    cmd=block_device_has_right_size,
+                )
+                assert exit_code == 0, device.path + ': ' + output.decode()
+                assert output == '1048576'
+            finally:
+                new_container.stop()
+                new_container.remove()
 
     def test_labels(self) -> None:
         """
