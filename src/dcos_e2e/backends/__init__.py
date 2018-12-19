b88343130b3c6e391299c995072ffeb7047799b4
Adam Dangoor <adamdangoor@gmail.com>
2018-12-19 14:04:24 +0000

    Fix sort order

--- src/dcos_e2e/backends/__init__.py
+++ src/dcos_e2e/backends/__init__.py
@@ -1,11 +1,14 @@
 """
 Classes to allow backend-specific configuration for cluster backend types.
+
+We skip sorting this file because import order is important.
+isort:skip_file
 """
 
 from ._aws import AWS
 from ._base_classes import ClusterBackend, ClusterManager
-from ._docker import Docker
 from ._existing_cluster import ExistingCluster as _ExistingCluster
+from ._docker import Docker
 from ._vagrant import Vagrant
 
 __all__ = [
