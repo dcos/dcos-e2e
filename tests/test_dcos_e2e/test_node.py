faf486f0a3300bbbc33dafdb393d81b0353898ac
Adam Dangoor <adamdangoor@gmail.com>
2019-01-25 19:38:25 +0000

    Progress towards using tmp_path rather than tmpdir

--- tests/test_dcos_e2e/test_node.py
+++ tests/test_dcos_e2e/test_node.py
@@ -19,9 +19,6 @@ import pytest
 from _pytest.capture import CaptureFixture
 from _pytest.fixtures import SubRequest
 from _pytest.logging import LogCaptureFixture
-# See https://github.com/PyCQA/pylint/issues/1536 for details on why the errors
-# are disabled.
-from py.path import local  # pylint: disable=no-name-in-module, import-error
 
 from dcos_e2e.backends import Docker
 from dcos_e2e.cluster import Cluster
