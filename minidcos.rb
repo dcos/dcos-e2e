ea304ef6a1cbc38bce8fd21dbf0f21dfb1ac119b
Adam Dangoor <adamdangoor@gmail.com>
2019-05-03 11:00:41 +0000

    Update for release 2019.05.03.0

--- minidcos.rb
+++ minidcos.rb
@@ -1,7 +1,7 @@
 class Minidcos < Formula
   include Language::Python::Virtualenv
 
-  url "https://codeload.github.com/dcos/dcos-e2e/legacy.tar.gz/2019.05.01.0"
+  url "https://codeload.github.com/dcos/dcos-e2e/legacy.tar.gz/2019.05.03.0"
   head "https://github.com/dcos/dcos-e2e.git"
   homepage "http://minidcos.readthedocs.io/en/latest/"
   depends_on "python3"
