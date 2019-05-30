be678b4efe8eca635687ca6f599a3b0e66fdc293
Adam Dangoor <adamdangoor@gmail.com>
2019-05-30 17:22:02 +0100

    Update Docker backend to completely expect that 1.13.1 is not the default

--- src/dcos_e2e/backends/_docker/_containers.py
+++ src/dcos_e2e/backends/_docker/_containers.py
@@ -35,6 +35,7 @@ def _docker_service_file(
         DockerVersion.v1_11_2: '/usr/bin/docker daemon',
         DockerVersion.v1_13_1: '/usr/bin/docker daemon',
         DockerVersion.v17_12_1_ce: '/usr/bin/dockerd',
+        DockerVersion.v18_06_3_ce: '/usr/bin/dockerd',
     }[docker_version]
 
     docker_cmd = (
