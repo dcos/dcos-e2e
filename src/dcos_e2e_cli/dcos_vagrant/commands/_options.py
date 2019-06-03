0e9093ce5d0487fd89f26c012b7b42bbba4064e1
Adam Dangoor <adamdangoor@gmail.com>
2019-06-03 10:00:43 +0100

    Progress towards reducing duplication

--- src/dcos_e2e_cli/dcos_vagrant/commands/_options.py
+++ src/dcos_e2e_cli/dcos_vagrant/commands/_options.py
@@ -6,15 +6,18 @@ from typing import Callable
 
 import click
 
+from dcos_e2e.backends import Vagrant
+
 
 def vm_memory_mb_option(command: Callable[..., None]) -> Callable[..., None]:
     """
     An option decorator for the amount of memory given to each VM.
     """
+    backend = Vagrant()
     function = click.option(
         '--vm-memory-mb',
         type=click.INT,
-        default=2048,
+        default=backend.vagrant_memory_mb,
         show_default=True,
         help='The amount of memory to give each VM.',
     )(command)  # type: Callable[..., None]
