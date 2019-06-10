8509a89e81a5d6ecd8877ab59503d7727928890b
Adam Dangoor <adamdangoor@gmail.com>
2019-06-10 12:11:54 +0100

    Remove unnecessary change

--- src/dcos_e2e/node.py
+++ src/dcos_e2e/node.py
@@ -695,7 +695,6 @@ class Node:
             dcos_config: The contents of the DC/OS ``config.yaml``.
             ip_detect_path: The path to the ``ip-detect`` script to use for
                 installing DC/OS.
-                node=self,
             role: The desired DC/OS role for the installation.
             user: The username to communicate as. If ``None`` then the
                 ``default_user`` is used instead.
